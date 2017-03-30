f7496a257f5ffd557a17a810a5f8db8822841bd0
a14017 <chang.maz@athena.com.tw>
2017-03-30 12:59:00 +0800

    多語系修改 規則檔案轉移

--- ruleEngine/rules/PMS0810040Rule.js
+++ ruleEngine/rules/PMS0810040Rule.js
@@ -1,8 +1,7 @@
 /**
- * Created by Jun Chang on 2017/3/7.
- * 訂房規則
+ * Created by Jun on 2017/3/30.
+ * 州別對照檔規則
  */
-
 var _ = require("underscore");
 var moment = require("moment");
 var async = require("async");
@@ -12,18 +11,6 @@ var ReturnClass = require("../returnClass");
 var ErrorClass = require("../errorClass");
 
 module.exports = {
-    /**
-     * 房間大類新增時預設值
-     * @param postData {Object} : 使用者傳來的資料
-     * @param session {Object}  : Session
-     * @param callback {Function} :
-     */
-    room_rf_add: function (postData, session, callback) {
-        var result = new ReturnClass();
-        var error = null;
-        result.defaultValues = commandRules.getCreateCommonDefaultDataRule(session);
-        callback(error, result);
-    },
     /**
      * 洲別新增預設值
      * @param postData {Object} : 使用者傳來的資料
