8dd0dc19de63dd42124f3a9edba63d004f447187
a14020 <wite15@athena.com.tw>
2017-04-13 13:48:12 +0800

    加上PMS0810110 對照檔(未完成)

--- ruleEngine/rules/PMS0810110Rule.js
+++ ruleEngine/rules/PMS0810110Rule.js
@@ -10,46 +10,135 @@ var commandRules = require("./commonRule");
 var ReturnClass = require("../returnClass");
 var ErrorClass = require("../errorClass");
 
-module.exports ={
-    chk_source_grp_rf : function (postData,session,callback) {
+module.exports = {
+    chk_source_grp_rf: function (postData, session, callback) {
         var lo_result = new ReturnClass();
         var lo_error = null;
-        var params={
-            athena_id : session.user.athena_id,
-            source_grp : postData.rowData.source_grp
+        var params = {
+            athena_id: session.user.athena_id,
+            source_grp: postData.rowData.source_grp
         };
 
-        if(!_.isEmpty(postData.rowData.source_grp.trim())) {
-            queryAgent.query("CHK_SOURCE_GRP_RF".toUpperCase(),params,function (err,guestData) {
-                if(!err){
-                    if(guestData.source_count == 0){
+        if (!_.isEmpty(postData.rowData.source_grp.trim())) {
+            queryAgent.query("CHK_SOURCE_GRP_RF".toUpperCase(), params, function (err, guestData) {
+                if (!err) {
+                    if (guestData.source_count == 0) {
                         lo_error = new ErrorClass();
                         lo_result.success = false;
                         lo_error.errorMsg = "此群組代號不在群組中";
-                        lo_error.errorCod="1111";
+                        lo_error.errorCod = "1111";
 
                         lo_result.effectValues = postData.rowData;
 
-                        callback(lo_error,lo_result);
-                    }else {
+                        callback(lo_error, lo_result);
+                    } else {
                         lo_error = new ErrorClass();
                         lo_result.success = true;
                         postData.rowData.source_grp = postData.newValue;
                         lo_result.effectValues = postData.rowData;
 
-                        callback(lo_error,lo_result);
+                        callback(lo_error, lo_result);
 
                     }
-                }else {
+                } else {
                     lo_error = new ErrorClass();
                     lo_result.success = false;
                     lo_error.errorMsg = err;
-                    lo_error.errorCod="1111";
+                    lo_error.errorCod = "1111";
 
-                    callback(err,lo_result);
+                    callback(err, lo_result);
                 }
 
             })
         }
+    },
+    chkSourcerfModifysta: function (postData, session, callback) {
+        var lo_result = new ReturnClass();
+        var lo_error = null;
+        var params = {
+            athena_id: session.user.athena_id,
+            source_grp: postData.singleRowData.source_grp
+        };
+
+        var createSubFunc = [];
+        var isDeleteRow = false;
+
+            createSubFunc.push(
+                function (callback) {
+                    async.waterfall([
+                        function (callback) {
+                            var modifySta = postData.singleRowData.modify_sta.trim();
+                            if (modifySta == "N") {
+                                isDeleteRow = false;
+                                callback(isDeleteRow, []);
+                            } else {
+                                isDeleteRow = true;
+                                callback(isDeleteRow, []);
+                            }
+                        },
+                        function (data, callback) {
+                            queryAgent.query("GET_ORDER_MN.GUEST_TYP_COUNT".toUpperCase(), params, function (err, guestData) {
+                                if (!err) {
+                                    if (data == true) {
+                                        if (guestData.guest_count > 0) {
+                                            isDeleteRow = false;
+                                            callback(isDeleteRow, []);
+                                        } else {
+                                            isDeleteRow = true;
+                                            callback(isDeleteRow, []);
+                                        }
+                                    } else {
+                                        callback(data, []);
+                                    }
+                                } else {
+                                    callback(err, []);
+                                }
+                            })
+                        }, function (data, callback) {
+                            queryAgent.query("GET_GW_CUST_RF.DEFAULT_GUEST_TYP_COUNT".toUpperCase(), params, function (err, guestData) {
+                                if (!err) {
+                                    if (data == true) {
+                                        if (guestData.guest_count > 0) {
+                                            isDeleteRow = false;
+                                            callback(isDeleteRow, []);
+                                        } else {
+                                            isDeleteRow = true;
+                                            callback(isDeleteRow, []);
+                                        }
+                                    } else {
+                                        callback(data, []);
+                                    }
+                                } else {
+                                    callback(err, []);
+                                }
+                            })
+                        }
+                        // , function (data, callback) {  SAM 等待星光大哥確認package
+                        //
+                        // }
+                    ], function (errMsg, result) {
+                        if (errMsg) {
+                            lo_error = new ErrorClass();
+                            lo_result.success = false;
+                            if (errMsg == false)
+                                lo_error.errorMsg = "已經有使用到此類別，不能刪除";
+                            else
+                                lo_error.errorMsg = errMsg;
+
+                            lo_error.errorCod = "1111";
+                            callback(lo_error, lo_result);
+
+                        } else {
+                            callback(lo_error, lo_result);
+                        }
+                    })
+                }
+            );
+
+            async.parallel(createSubFunc, function (err, result) {
+                callback(err, result);
+            })
+
+
     }
 }
