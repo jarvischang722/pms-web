53b09cab514c57d61e10d878767c8d541c6d7365
a14017 <chang.maz@athena.com.tw>
2017-04-24 23:31:38 +0800

    ruleAgent load 檔改為多階層撈取

--- ruleEngine/rules/SetUp/PMS0810090Rule.js
+++ ruleEngine/rules/SetUp/PMS0810090Rule.js
@@ -4,10 +4,13 @@
 var _ = require("underscore");
 var moment = require("moment");
 var async = require("async");
-var queryAgent = require('../../plugins/kplug-oracle/QueryAgent');
-//var commandRules = require("./CommonRule");
-var ReturnClass = require("../returnClass");
-var ErrorClass = require("../errorClass");
+var path = require('path');
+var appRootDir = path.dirname(require.main.filename);
+var ruleRootPath = appRootDir+"/ruleEngine/";
+var queryAgent = require(appRootDir+'/plugins/kplug-oracle/QueryAgent');
+var commandRules = require("./../CommonRule");
+var ReturnClass = require(ruleRootPath+"/returnClass");
+var ErrorClass = require(ruleRootPath+"/errorClass");
 
 module.exports = {
     chk_guest_rf_guest_way: function (postData, session, callback) {
--- ruleEngine/rules/SetUp/PMS0810140Rule.js
+++ ruleEngine/rules/SetUp/PMS0810140Rule.js
@@ -4,10 +4,13 @@
 var _ = require("underscore");
 var moment = require("moment");
 var async = require("async");
-var queryAgent = require('../../plugins/kplug-oracle/QueryAgent');
-var commandRules = require("./CommonRule");
-var ReturnClass = require("../returnClass");
-var ErrorClass = require("../errorClass");
+var path = require('path');
+var appRootDir = path.dirname(require.main.filename);
+var ruleRootPath = appRootDir+"/ruleEngine/";
+var queryAgent = require(appRootDir+'/plugins/kplug-oracle/QueryAgent');
+var commandRules = require("./../CommonRule");
+var ReturnClass = require(ruleRootPath+"/returnClass");
+var ErrorClass = require(ruleRootPath+"/errorClass");
 
 module.exports = {
     //FOC_DT.FREE_TYP已經用到的,此筆資料不可刪除
