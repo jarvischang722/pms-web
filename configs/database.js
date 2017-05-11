1d85dd0da6098cf80d9f3c740375299866739d98
a16010 <lai.kaiyue@athena.com.tw>
2017-05-11 19:30:58 +0800

    1. PMS0830020實作中

--- configs/database.js
+++ configs/database.js
@@ -5,18 +5,18 @@
 module.exports = {
     //oracle 設定檔{Array}
     oracle: [{
-        id: 'IDC_TEST_ORACLE',
-        connectString: "210.64.24.131/WRS",
-        // connectString: "192.168.168.203/MISUTF8",
+        id: 'o1',
+        connectString: "192.168.168.203/MISUTF8",
         user: "htce7",
         password: "sql",
         months: [1, 2, 3, 4, 5, 6],
-        debug: 1
-    }],
+        debug: 0
+    }]
+    ,
     //mongoDB 設定檔
     mongo: {
-        // host: "192.168.1.76",
-        host: "127.0.0.1",
+        host: "192.168.1.76",
+        // host: "127.0.0.1",
         port: "2717",
         dbname: "bacchus",
         username: "bacchus",
