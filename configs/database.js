316995f45de207e8e88f68cc2ac2d0cbbf8c7fe9
a16010 <lai.kaiyue@athena.com.tw>
2017-05-12 18:49:53 +0800

    1. PMS0830020實作中

--- configs/database.js
+++ configs/database.js
@@ -5,19 +5,20 @@
 module.exports = {
     //oracle 設定檔{Array}
     oracle: [{
-        id: 'o1',
-        connectString: "192.168.168.203/MISUTF8",
+        id: 'IDC_TEST_ORACLE',
+        connectString: "210.64.24.131/WRS",
         user: "htce7",
         password: "sql",
         months: [1, 2, 3, 4, 5, 6],
-        debug: 0
+        debug: 1
     }]
     ,
     //mongoDB 設定檔
     mongo: {
         host: "192.168.1.76",
-        // host: "127.0.0.1",
         port: "2717",
+        // host: "127.0.0.1",
+        // port: "27017",
         dbname: "bacchus",
         username: "bacchus",
         password: "mongo"
--- ruleEngine/rules/SetUp/PMS0830020Rule.js
+++ ruleEngine/rules/SetUp/PMS0830020Rule.js
@@ -1,3 +1,117 @@
 /**
  * Created by a16010 on 2017/5/11.
+ * 程式代碼: PMS0830020, 服務種類設定
  */
+
+var _ = require("underscore");
+var moment = require("moment");
+var async = require("async");
+var path = require('path');
+var appRootDir = path.dirname(require.main.filename);
+var ruleRootPath = appRootDir+"/ruleEngine/";
+var queryAgent = require(appRootDir+'/plugins/kplug-oracle/QueryAgent');
+var commandRules = require("./../CommonRule");
+var ReturnClass = require(ruleRootPath+"/returnClass");
+var ErrorClass = require(ruleRootPath+"/errorClass");
+
+module.exports = {
+    chkUsetyp: function (postData, session, callback){
+        var lo_result = new ReturnClass();
+        var lo_error = null;
+        var params = {
+            athena_id: session.user.athena_id,
+            modify_sta: postData.singleRowData.modify_sta
+        };
+
+        var createSubFunc = [];
+        var isDeleteRow = false;
+    },
+
+    chkSourcerfModifysta: function (postData, session, callback) {
+        var lo_result = new ReturnClass();
+        var lo_error = null;
+        var params = {
+            athena_id: session.user.athena_id,
+            source_grp: postData.singleRowData.source_grp
+        };
+
+        var createSubFunc = [];
+        var isDeleteRow = false;
+
+        createSubFunc.push(
+            function (callback) {
+                async.waterfall([
+                    function (callback) {
+                        var modifySta = postData.singleRowData.modify_sta.trim();
+                        if (modifySta == "N") {
+                            isDeleteRow = false;
+                            callback(null, isDeleteRow);
+                        } else {
+                            isDeleteRow = true;
+                            callback(null, isDeleteRow);
+                        }
+                    },
+                    function (data, callback) {
+                        queryAgent.query("GET_ORDER_MN.GUEST_TYP_COUNT".toUpperCase(), params, function (err, guestData) {
+                            if (!err) {
+                                if (data == true) {
+                                    if (guestData.guest_count > 0) {
+                                        isDeleteRow = false;
+                                        callback(null, isDeleteRow);
+                                    } else {
+                                        isDeleteRow = true;
+                                        callback(null, isDeleteRow);
+                                    }
+                                } else {
+                                    callback(null, data);
+                                }
+                            } else {
+                                callback(err, []);
+                            }
+                        })
+                    }, function (data, callback) {
+                        queryAgent.query("GET_GW_CUST_RF.DEFAULT_GUEST_TYP_COUNT".toUpperCase(), params, function (err, guestData) {
+                            if (!err) {
+                                if (data == true) {
+                                    if (guestData.guest_count > 0) {
+                                        isDeleteRow = false;
+                                        callback(null, isDeleteRow);
+                                    } else {
+                                        isDeleteRow = true;
+                                        callback(null, isDeleteRow);
+                                    }
+                                } else {
+                                    callback(null, data);
+                                }
+                            } else {
+                                callback(err, []);
+                            }
+                        })
+                    }
+                    // , function (data, callback) {  SAM 等待星光大哥確認package
+                    //
+                    // }
+                ], function (errMsg, result) {
+                    if (errMsg == null) {
+
+                        if (result == false) {
+                            lo_error = new ErrorClass();
+                            lo_result.success = false;
+                            lo_error.errorCod = "1111";
+                            lo_error.errorMsg = "已經有使用到此類別，不能刪除";
+                        }
+
+                        callback(lo_error, lo_result);
+
+                    } else {
+                        callback(lo_error, lo_result);
+                    }
+                })
+            }
+        );
+
+        async.parallel(createSubFunc, function (err, result) {
+            callback(err, result);
+        })
+    }
+}
