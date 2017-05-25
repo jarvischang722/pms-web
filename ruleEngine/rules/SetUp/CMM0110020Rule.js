91ffea8d9f12ee0886d873c36bc69bd12c8c638e
a14020 <wite15@athena.com.tw>
2017-05-25 14:58:26 +0800

    SAM:PMS0810030(國籍設定)(頁面出來但設定需調整)           CMM0110010(聯絡設定)(已完成但"系統"還不能進去看到表單)           CMM0110020(地址設定)(已完成但"系統"還不能進去看到表單)

---
+++ ruleEngine/rules/SetUp/CMM0110020Rule.js
@@ -0,0 +1,27 @@
+/**
+ * Created by a14020 on 2017/5/24.
+ */
+var _ = require("underscore");
+var moment = require("moment");
+var async = require("async");
+var path = require('path');
+var appRootDir = path.dirname(require.main.filename);
+var ruleRootPath = appRootDir+"/ruleEngine/";
+var queryAgent = require(appRootDir+'/plugins/kplug-oracle/QueryAgent');
+var commandRules = require("./../CommonRule");
+var ReturnClass = require(ruleRootPath+"/returnClass");
+var ErrorClass = require(ruleRootPath+"/errorClass");
+
+module.exports = {
+    chkAddressrfContacttyp: function (postData, session, callback) {
+        var error = null;
+        var result = new ReturnClass();
+        callback(error, result);
+
+    },
+    chk_address_rf_del:function (postData, session, callback) {
+        var error = null;
+        var result = new ReturnClass();
+        callback(error, result);
+    }
+}
