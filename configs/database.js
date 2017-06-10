729b4563df9708e6a8babc63dfd164cbee291dcf
m15009 <jwc1116@athena.com.tw>
2017-06-10 14:20:00 +0800

    ** 版型 170610 訂房卡介面 ** 1.費用列表 2.選項option 3.住客明細 4.指定房組 5.櫃檯備品

--- configs/database.js
+++ configs/database.js
@@ -7,6 +7,7 @@ module.exports = {
     oracle: [{
         id: 'IDC_TEST_ORACLE',
         connectString: "210.64.24.131/WRS",
+        // connectString: "192.168.168.203/MISUTF8",
         user: "htce7",
         password: "sql",
         months: [1, 2, 3, 4, 5, 6],
@@ -14,8 +15,8 @@ module.exports = {
     }],
     //mongoDB 設定檔
     mongo: {
-        // host: "192.168.1.76",
-        host: "127.0.0.1",
+        host: "192.168.1.76",
+        // host: "127.0.0.1",
         port: "2717",
         dbname: "bacchus",
         username: "bacchus",
