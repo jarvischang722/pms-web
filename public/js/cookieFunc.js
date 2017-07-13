3bcba0256d603ecf5e8594acefa1907b09f203cf
a14017 <chang.maz@athena.com.tw>
2017-07-13 16:38:05 +0800

    Table lock 修正

--- public/js/cookieFunc.js
+++ public/js/cookieFunc.js
@@ -8,20 +8,20 @@
  * @param path　: cookie　存在網址
  * @param keep_time {number} :　有效時間保存
  */
-function doCookieSetup(name, value, path , keep_time) {
+function setupCookie(name, value, path, keep_time) {
 
-    if(_.isUndefined(path)　|| _.isEmpty(path)){
-        path = "/"
+    if (_.isUndefined(path) || _.isEmpty(path)) {
+        path = "/";
     }
 
-    if(_.isUndefined(keep_time) || _.isNaN(Number(keep_time)) ){
+    if (_.isUndefined(keep_time) || _.isNaN(Number(keep_time))) {
         keep_time = 86400000;  //預設1天
     }
 
     var expires = new Date();
     //有效時間保存  1*24*60*60*1000 (天 * 時 * 分 * 秒  * 毫秒)
     expires.setTime(expires.getTime() + keep_time);
-    document.cookie = name.trim() + "=" + escape(value) + ";expires=" + expires.toGMTString() +";path="+path
+    document.cookie = name.trim() + "=" + escape(value) + ";expires=" + expires.toGMTString() + ";path=" + path;
 }
 
 /** 查詢 Get cookie by name **/
@@ -32,16 +32,22 @@ function getCookie(name) {
     var i = 0;
     while (i < cookieLen) {
         var j = i + nameLen;
-        if (document.cookie.substring(i, j) == arg) return getCookieValueByIndex(j);
+        if (document.cookie.substring(i, j) == arg) {
+            return getCookieValueByIndex(j);
+        }
         i = document.cookie.indexOf(" ", i) + 1;
-        if (i == 0) break;
+        if (i == 0) {
+            break;
+        }
     }
     return null;
 }
 
 function getCookieValueByIndex(startIndex) {
     var endIndex = document.cookie.indexOf(";", startIndex);
-    if (endIndex == -1) endIndex = document.cookie.length;
+    if (endIndex == -1) {
+        endIndex = document.cookie.length;
+    }
     return unescape(document.cookie.substring(startIndex, endIndex));
 }
 
