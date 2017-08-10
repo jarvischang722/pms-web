e31b1686e3cbcb0309e7a0e81fe7bda8ddd237ea
m15009 <jwc1116@athena.com.tw>
2017-08-10 14:51:50 +0800

    ** 版型 170810 靜態介面修改 ** 1.<Profile -> 櫃檯備品> table修改 2.<Cashier Departure> 3.<Room Type Plan> 4.住客帳資料 5.所有的搜尋視窗拉大 6.新增 Deposits 7.新增 櫃檯備品

--- routes/reservation.js
+++ routes/reservation.js
@@ -50,6 +50,9 @@ module.exports = function(app  ) {
     //櫃檯備品iframe(靜態)
     app.get('/resv_ratePlanTable', reservationCrtl.getResv_ratePlanTable);
 
+    //amenitiesIny. 櫃檯備品iframe(靜態)
+    app.get('/resv_amenitiesIframe', reservationCrtl.getResv_amenitiesIframe);
+
     //交辦事項(靜態)
     app.get('/PMS0120070', reservationCrtl.getPMS0120070);
 
