34f37d312ca37cbec3ce010f83aead855ad4c784
a16010 <a16010@athena.com.tw>
2017-08-15 15:16:24 +0800

    PMS0860030 暫存

--- public/js/setup/PMS0860030_setup.js
+++ public/js/setup/PMS0860030_setup.js
@@ -4,6 +4,85 @@
  * 程式名稱: 區域別設定
  */
 
+var prg_id = $("#prg_id").val();
+var vmHub = new Vue;
+
+var vm = new Vue({
+    el: "#app",
+    mounted: function(){
+        this.fetchUserInfo();
+        this.loadDataGridByPrgID(function (success) {
+        });
+    },
+    data: {
+        userInfo: ""
+    },
+    methods: {
+        //取得使用者資料
+        fetchUserInfo: function () {
+            $.post('/api/getUserInfo', function (result) {
+                if (result.success) {
+                    vm.userInfo = result.userInfo;
+                }
+            });
+        }
+    }
+});
+
+var jsonData = [
+    {
+        id  : 1,
+        text : "Folder 1",
+        state : {
+            selected  : false
+        },
+        children    : [
+            {
+                id  : 2,
+                text : "Sub Folder 1",
+                state : {
+                    selected  : false
+                },
+                children: [
+                    {
+                        id: 5,
+                        text: "sub folder child 1",
+                        state: {
+                            selected: false
+                        }
+                    }
+                ]
+            },
+            {
+                id  : 3,
+                text : "Sub Folder 2",
+                state : {
+                    selected  : false
+                }
+            }
+        ]
+    },
+    {
+        id: 4,
+        text : "Folder 2",
+        state : {
+            selected : true
+        },
+        children : []
+    }
+];
+
+// areaTree demo
+$('#areaTree').jstree({
+    "core" : {
+        "animation" : 0,
+        "check_callback" : true,
+        "themes" : { "stripes" : true },
+        'data' : jsonData
+    },
+    "plugins" : [ "dnd", "search", "state", "types", "wholerow" ]
+});
+
 function areaTree_create() {
     var ref = $('#areaTree').jstree(true),
         sel = ref.get_selected();
@@ -27,21 +106,5 @@ function areaTree_delete() {
     if(!sel.length) { return false; }
     ref.delete_node(sel);
 }
-
-// areaTree demo
-$('#areaTree').jstree({
-    "core" : {
-        "animation" : 0,
-        "themes" : { "stripes" : true },
-        "check_callback" : true
-    },
-    "types" : {
-        "#" : { "max_children" : 1, "max_depth" : 4, "valid_children" : ["root"] },
-        "root" : { "icon" : "/images/icon/taiwan.png", "valid_children" : ["default"] },
-        "default" : {"icon" : "/images/icon/taiwan.png",  "valid_children" : ["default","file"] },
-        "file" : { "icon" : "fa fa-user", "valid_children" : [] }
-    },
-    "plugins" : [ "contextmenu", "dnd", "search", "state", "types", "wholerow" ]
-});
 
 
