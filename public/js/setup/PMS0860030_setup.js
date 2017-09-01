73f7dff3e03f59dcc4a80a5fa64befb867365dd6
a16010 <a16010@athena.com.tw>
2017-09-01 19:05:14 +0800

    PMS0860030 bug修改

--- public/js/setup/PMS0860030_setup.js
+++ public/js/setup/PMS0860030_setup.js
@@ -10,267 +10,266 @@ var ga_treeData = [];
 var gs_action = null;
 
 var vm = new Vue({
-    el: "#app",
-    mounted: function () {
-        this.fetchUserInfo();
-        this.loadDataGridByPrgID();
-    },
-    data: {
-        dataGridRows: [],
-        fieldData: [],
-        userInfo: "",
-        treeData: {},
-        tree: null,
-        tmpCud: {               //新刪修暫存
-            createData: [],
-            updateData: [],
-            deleteData: []
+        el: "#app",
+        mounted: function () {
+            this.fetchUserInfo();
+            this.loadDataGridByPrgID();
         },
-        maxAreaCod: null
-    },
-    methods: {
-        //取得使用者資料
-        fetchUserInfo: function () {
-            $.post('/api/getUserInfo', function (result) {
-                if (result.success) {
-                    vm.userInfo = result.userInfo;
+        data: {
+            dataGridRows: [],
+            fieldData: [],
+            userInfo: "",
+            treeData: {},
+            tree: null,
+            tmpCud: {               //新刪修暫存
+                createData: [],
+                updateData: [],
+                deleteData: []
+            },
+            maxAreaCod: null,
+            isTreeCreate: false
+        },
+        methods: {
+            //取得使用者資料
+            fetchUserInfo: function () {
+                $.post('/api/getUserInfo', function (result) {
+                    if (result.success) {
+                        vm.userInfo = result.userInfo;
+                    }
+                });
+            },
+
+            initTemCud: function () {
+                this.tmpCud.createData = [];
+                this.tmpCud.updateData = [];
+                this.tmpCud.deleteData = [];
+                // $("#areaTree").jstree("destroy");
+            },
+
+            //抓取顯示資料
+            loadDataGridByPrgID: function () {
+                var self = this;
+                $.post("/api/prgDataGridDataQuery", {prg_id: gs_prg_id}, function (result) {
+                    waitingDialog.hide();
+                    vm.dataGridRows = _.map(result.dataGridRows, function (obj) {
+                        if (obj.area_cod.trim() != "ROOT") {
+                            var li_area_cod = Number(obj.area_cod);
+                            if (_.isNaN(li_area_cod)) {
+                                obj.area_cod = padLeft(obj.area_cod, 3);
+                            }
+                            else {
+                                obj.area_cod = padLeft(obj.area_cod, 4);
+                            }
+                        }
+                        return _.clone(obj);
+                    });
+                    vm.fieldData = result.fieldData;
+                    if (vm.dataGridRows.length != 0) {
+                        result.dataGridRows = _.without(result.dataGridRows, _.findWhere(result.dataGridRows, {area_cod: "ROOT"}));
+                        self.maxAreaCod = _.sortBy(result.dataGridRows, "area_cod");
+                        self.maxAreaCod = self.maxAreaCod[self.maxAreaCod.length - 1].area_cod;
+                    }
+                    else {
+                        self.maxAreaCod = "0000";
+                    }
+
+                    vm.initAreaTree();
+                });
+            },
+
+            // 初始化jstree
+            initAreaTree: function () {
+                this.convertDataGridRows2TreeData();
+                if(this.isTreeCreate == false){
+                    this.createTree();
+                    this.tree = $("#areaTree").jstree(true);
+                    this.isTreeCreate = true;
                 }
-            });
-        },
+                else{
+                    this.tree.settings.core.data = vm.treeData.root;
+                    this.tree.refresh();
+                }
+            },
 
-        initTemCud: function(){
-            this.tmpCud.createData = [];
-            this.tmpCud.updateData = [];
-            this.tmpCud.deleteData = [];
-        },
+            // 將資料轉換成jstree格式
+            convertDataGridRows2TreeData: function () {
+                var self = this;
+                var lo_rootDataRow = {};
 
-        //抓取顯示資料
-        loadDataGridByPrgID: function () {
-            var self = this;
-            $.post("/api/prgDataGridDataQuery", {prg_id: gs_prg_id}, function (result) {
-                waitingDialog.hide();
-                vm.dataGridRows = _.map(result.dataGridRows, function (obj) {
-                    if (obj.area_cod.trim() != "ROOT") {
-                        var li_area_cod = Number(obj.area_cod);
-                        if (_.isNaN(li_area_cod)) {
-                            obj.area_cod = padLeft(obj.area_cod, 3);
+                var lo_rootNode = _.findWhere(vm.dataGridRows, {area_cod: "ROOT"});
+                if (_.isUndefined(lo_rootNode)) {
+                    lo_rootDataRow.area_cod = "ROOT";
+                    lo_rootDataRow.sort_cod = 0;
+                    lo_rootDataRow.area_nam = "區域別";
+                    lo_rootDataRow.parent_cod = "#";
+                    this.treeData = new Tree(lo_rootDataRow);
+                }
+                else {
+                    this.treeData = new Tree(lo_rootNode);
+                }
+
+                searchChildAndInsert(this.treeData.root);
+            },
+
+            createTree: function () {
+                // areaTree demo
+
+                $('#areaTree').jstree({
+                    "core": {
+                        "animation": 0,
+                        "themes": {"stripes": true},
+                        "multiple": false,                  //不可多選
+                        "check_callback": true,
+                        "data": vm.treeData.root
+                    },
+                    "checkbox": {
+                        "keep_selected_style": true,
+                        "whole_node": false,
+                        "tie_selection": false               // 選取時，false只會選到父節點，不會選到子結點
+                    },
+                    "dnd": {
+                        "dnd": true,
+                        "drag_selection": true,
+                        "check_while_dragging": true
+                    },
+                    "plugins": ["dnd", "search", "state", "types", "wholerow", "checkbox"]
+                });
+            },
+
+            tmpCudHandler: function (currNode) {
+                var self = this;
+                var lo_parentNode = this.tree.get_node(this.tree.get_parent(currNode));
+
+                // 取排序
+                if (lo_parentNode.children.length != 0) {
+                    _.each(lo_parentNode.children, function (childNode, Idx) {
+                        var lo_childNode = self.tree.get_node(childNode);
+                        lo_childNode.position = Idx;
+                        if (lo_childNode.createStatus == "N" || lo_childNode.deleteStatus == "N") {
+                            lo_childNode.updateStatus = "Y";
+                        }
+                    });
+                }
+
+                _.each(lo_parentNode.children, function (childNode) {
+                    var lo_childNode = self.tree.get_node(childNode);
+                    // 去除暫存重複
+                    _.each(self.tmpCud, function (obj, key) {
+                        if (obj.length != 0) {
+                            self.tmpCud[key] = _.without(obj, _.findWhere(obj, {
+                                area_cod: childNode
+                            }));
+                        }
+                    });
+                    if (lo_childNode.createStatus == "Y") {
+                        self.tmpCud["createData"].push(new rowData(lo_childNode));
+                    }
+                    if (lo_childNode.updateStatus == "Y") {
+                        self.tmpCud["updateData"].push(new rowData(lo_childNode));
+                    }
+                    if (lo_childNode.deleteStatus == "Y") {
+                        self.tmpCud["deleteData"].push(new rowData(lo_childNode));
+                    }
+                });
+            },
+
+            doSave: function () {
+                var self = this;
+                var fieldData = [
+                    {ui_field_name: 'athena_id', keyable: 'Y'},
+                    {ui_field_name: "area_cod", keyable: "Y"}
+                ];
+
+                var params = {
+                    prg_id: gs_prg_id,
+                    tmpCUD: this.tmpCud,
+                    fieldData: fieldData,
+                    mainTableName: "area_cod_kvrf"
+                };
+                $.post('/api/execSQLProcess', params)
+                    .done(function (response) {
+                        if (response.success) {
+                            self.initTemCud();
+                            self.loadDataGridByPrgID();
+                            alert('save success!');
                         }
                         else {
-                            obj.area_cod = padLeft(obj.area_cod, 4);
+                            alert(response.errorMsg);
                         }
-                    }
-                    return _.clone(obj);
-                });
-                vm.fieldData = result.fieldData;
-                if (vm.dataGridRows.length != 0) {
-                    result.dataGridRows = _.without(result.dataGridRows, _.findWhere(result.dataGridRows, {area_cod: "ROOT"}));
-                    self.maxAreaCod = _.sortBy(result.dataGridRows, "area_cod");
-                    self.maxAreaCod = self.maxAreaCod[self.maxAreaCod.length - 1].area_cod;
-                }
-                else {
-                    self.maxAreaCod = "0000";
-                }
-
-                vm.initAreaTree();
-            });
-        },
-
-        // 初始化jstree
-        initAreaTree: function () {
-            this.convertDataGridRows2TreeData();
-            this.createTree();
-            this.tree = $("#areaTree").jstree(true);
-        },
-
-        // 將資料轉換成jstree格式
-        convertDataGridRows2TreeData: function () {
-            var self = this;
-            var lo_rootDataRow = {};
-
-            var lo_rootNode = _.findWhere(vm.dataGridRows, {area_cod: "ROOT"});
-            if (_.isUndefined(lo_rootNode)) {
-                lo_rootDataRow.area_cod = "ROOT";
-                lo_rootDataRow.sort_cod = 0;
-                lo_rootDataRow.area_nam = "ROOT";
-                lo_rootDataRow.parent_cod = "#";
-                this.treeData = new Tree(lo_rootDataRow);
-            }
-            else {
-                this.treeData = new Tree(lo_rootNode);
-            }
-
-
-            searchChildAndInsert(this.treeData.root);
-        },
-
-        createTree: function () {
-            // areaTree demo
-
-            $('#areaTree').jstree({
-                "core": {
-                    "animation": 0,
-                    "themes": {"stripes": true},
-                    "multiple": false,                  //不可多選
-                    "check_callback": true,
-                    "data": vm.treeData.root
-                },
-                "checkbox": {
-                    "keep_selected_style": true,
-                    "whole_node": false,
-                    "tie_selection": false               // 選取時，false只會選到父節點，不會選到子結點
-                },
-                "dnd": {
-                    "dnd": true,
-                    "drag_selection": true,
-                    "check_while_dragging": true
-                },
-                "types": {
-                    "#": {"max_children": 1, "valid_children": ["root"]},
-                    "root": {"icon": "/images/icon/taiwan.png", "valid_children": ["default"]},
-                    "default": {"icon": "/images/icon/taiwan.png", "valid_children": ["default", "file"]},
-                    "file": {"icon": "fa fa-user", "valid_children": []}
-                },
-                "plugins": ["dnd", "search", "state", "types", "wholerow", "checkbox"]
-            });
-        },
-
-        tmpCudHandler: function (currNode) {
-            var self = this;
-            var lo_parentNode = this.tree.get_node(this.tree.get_parent(currNode));
-
-            // 取排序
-            if (lo_parentNode.children.length != 0) {
-                _.each(lo_parentNode.children, function (childNode, Idx) {
-                    var lo_childNode = self.tree.get_node(childNode);
-                    lo_childNode.position = Idx;
-                    if(lo_childNode.createStatus == "N" || lo_childNode.deleteStatus == "N"){
-                        lo_childNode.updateStatus = "Y";
-                    }
-                });
-            }
-
-            _.each(lo_parentNode.children, function (childNode) {
-                var lo_childNode = self.tree.get_node(childNode);
-                // 去除暫存重複
-                _.each(self.tmpCud, function (obj, key) {
-                    if (obj.length != 0) {
-                        self.tmpCud[key] = _.without(obj, _.findWhere(obj, {
-                            area_cod: childNode
-                        }));
-                    }
-                });
-                if (lo_childNode.createStatus == "Y") {
-                    self.tmpCud["createData"].push(new rowData(lo_childNode));
-                }
-                if (lo_childNode.updateStatus == "Y") {
-                    self.tmpCud["updateData"].push(new rowData(lo_childNode));
-                }
-                if (lo_childNode.deleteStatus == "Y") {
-                    self.tmpCud["deleteData"].push(new rowData(lo_childNode));
-                }
-            });
-        },
-
-        doSave: function () {
-            waitingDialog.show('Saving...');
-            var self = this;
-            var fieldData = [
-                {ui_field_name: 'athena_id', keyable: 'Y'},
-                {ui_field_name: "area_cod", keyable: "Y"}
-            ];
-
-            var params = {
-                prg_id: gs_prg_id,
-                tmpCUD: this.tmpCud,
-                fieldData: fieldData,
-                mainTableName: "area_cod_kvrf"
-            };
-            $.post('/api/execSQLProcess', params)
-                .done(function (response) {
-                    waitingDialog.hide();
-                    if (response.success) {
-                        self.initTemCud();
-                        self.loadDataGridByPrgID();
-                        alert('save success!');
-                    } else {
-                        alert(response.errorMsg);
-                    }
-                })
-                .fail(function (error) {
-                    waitingDialog.hide();
-                    console.log(error);
-                });
-        },
-
-        getSelectedNode: function () {
-            var sel = this.tree.get_selected()[0];
-            return sel;
-        },
-
-        addNode: function () {
-            var sel = this.getSelectedNode();
-            sel = this.tree.create_node(sel, {"type": "default"});
-            gs_action = "create";
-            if (sel) {
-                this.tree.edit(sel);
-            }
-        },
-
-        renameNode: function () {
-            var sel = this.getSelectedNode();
-            this.tree.edit(sel);
-            gs_action = "rename";
-        },
-
-        delNode: function () {
-            var self = this;
-            var lo_selNode = this.getSelectedNode();
-            var lo_node = this.tree.get_node(lo_selNode);
-            var lo_dgRow = _.findWhere(vm.dataGridRows, {area_cod: lo_selNode});
-
-            if(_.isUndefined(lo_dgRow)){
-                return true;
-            }
-
-            if(lo_node.createStatus == "Y"){
-                this.tmpCud["createData"] = _.without(this.tmpCud["createData"], _.findWhere(this.tmpCud["createData"], {
-                    area_cod: lo_node.id
-                }));
-                this.tree.delete_node(lo_selNode);
-                return true;
-            }
-
-            // 根節點不能刪除
-            if (lo_dgRow.parent_cod.trim() == "#") {
-                return true;
-            }
-
-            var la_allDelRowData = searchNode(lo_node, []);
-            la_allDelRowData.push(new rowData(lo_node));
-
-            $.post("/api/handleDataGridDeleteEventRule", {
-                prg_id: gs_prg_id,
-                deleteData: la_allDelRowData
-            }, function (result) {
-                if (result.success) {
-                    _.each(la_allDelRowData, function(delRowData){
-                        var lo_childNode = self.tree.get_node(delRowData.area_cod);
-                        lo_childNode.deleteStatus = "Y";
-                        self.tmpCudHandler(lo_childNode);
+                    })
+                    .fail(function (error) {
+                        console.log(error);
                     });
+            },
 
-                    self.tree.delete_node(lo_selNode);
+            getSelectedNode: function () {
+                var sel = this.tree.get_selected()[0];
+                return sel;
+            },
 
+            addNode: function () {
+                var sel = this.getSelectedNode();
+                sel = this.tree.create_node(sel, {"type": "default"});
+                gs_action = "create";
+                if (sel) {
+                    this.tree.edit(sel);
                 }
-                else {
-                    alert(result.errorMsg);
+            },
+
+            renameNode: function () {
+                var lo_selNode = this.getSelectedNode();
+                var lo_node = this.tree.get_node(lo_selNode);
+                if (lo_node.id == "ROOT") {
+                    return true;
                 }
-            });
+                this.tree.edit(lo_selNode);
+                gs_action = "rename";
+            },
 
+            delNode: function () {
+                var self = this;
+                var lo_selNode = this.getSelectedNode();
+                var lo_node = this.tree.get_node(lo_selNode);
+                var lo_dgRow = _.findWhere(vm.dataGridRows, {area_cod: lo_selNode});
 
+                // 根節點不能刪除
+                if (lo_node.id == "ROOT") {
+                    return true;
+                }
+
+                if (lo_node.createStatus == "Y") {
+                    this.tmpCud["createData"] = _.without(this.tmpCud["createData"], _.findWhere(this.tmpCud["createData"], {
+                        area_cod: lo_node.id
+                    }));
+                    this.tree.delete_node(lo_selNode);
+                    return true;
+                }
+
+                var la_allDelRowData = searchNode(lo_node, []);
+                la_allDelRowData.push(new rowData(lo_node));
+
+                $.post("/api/handleDataGridDeleteEventRule", {
+                    prg_id: gs_prg_id,
+                    deleteData: la_allDelRowData
+                }, function (result) {
+                    if (result.success) {
+                        _.each(la_allDelRowData, function (delRowData) {
+                            var lo_childNode = self.tree.get_node(delRowData.area_cod);
+                            lo_childNode.deleteStatus = "Y";
+                            self.tmpCudHandler(lo_childNode);
+                        });
+
+                        self.tree.delete_node(lo_selNode);
+
+                    }
+                    else {
+                        alert(result.errorMsg);
+                    }
+                });
+            }
         }
-    }
-});
+    })
+;
 
 function searchChildAndInsert(lo_node) {
 
