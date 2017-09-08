57d51185e26d56f50c4796eec49a33fb7e48806f
a16010 <a16010@athena.com.tw>
2017-09-08 15:48:39 +0800

    單筆format require 驗證功能完成

--- public/js/easyUI/extendValidation.js
+++ public/js/easyUI/extendValidation.js
@@ -15,48 +15,54 @@ $.extend($.fn.validatebox.defaults.rules, {
     //四碼英文數字
     FmtFourEngNum: {
         validator: function (value) {
-            $.fn.validatebox.defaults.rules.FmtFourEngNum.message = go_i18nLang.Validation.Formatter["FmtFourEngNum"]  ;
-            return go_validateClass.FmtFourEngNum(value).success;
+            var lo_checkResult = go_validateClass.chkLength(value);
+            $.fn.validatebox.defaults.rules.FmtFourEngNum.message = lo_checkResult.msg;
+            return lo_checkResult.success;
         },
         message: '格式為四碼英文數字.'
     },
     //數字文字
     FmtCharacters: {
         validator: function (value) {
-            $.fn.validatebox.defaults.rules.FmtCharacters.message = go_i18nLang.Validation.Formatter["FmtCharacters"]  ;
-            return go_validateClass.FmtCharacters(value).success;
+            var lo_checkResult = go_validateClass.FmtCharacters(value);
+            $.fn.validatebox.defaults.rules.FmtCharacters.message = lo_checkResult.msg;
+            return lo_checkResult.success;
         },
         message: '請輸入文字'
     },
     //一定要輸入兩碼
     FmtExactlyTwoWord: {
         validator: function (value) {
-            $.fn.validatebox.defaults.rules.FmtExactlyTwoWord.message = go_i18nLang.Validation.Formatter["FmtExactlyTwoWord"]  ;
-            return go_validateClass.FmtExactlyTwoWord(value).success;
+            var lo_checkResult = go_validateClass.FmtExactlyTwoWord(value);
+            $.fn.validatebox.defaults.rules.FmtExactlyTwoWord.message = lo_checkResult.msg;
+            return lo_checkResult.success;
         },
         message: '一定要輸入2碼'
     },
     //0到5碼數字
     FmtZeroToFiveNum: {
         validator: function (value) {
-            $.fn.validatebox.defaults.rules.FmtZeroToFiveNum.message = go_i18nLang.Validation.Formatter["FmtZeroToFiveNum"]  ;
-            return go_validateClass.FmtZeroToFiveNum(value).success;
+            var lo_checkResult = go_validateClass.FmtZeroToFiveNum(value);
+            $.fn.validatebox.defaults.rules.FmtZeroToFiveNum.message = lo_checkResult.msg;
+            return lo_checkResult.success;
         },
         message: '0到5碼數字'
     },
     //0到10碼數字
     FmtZeroToTenNum: {
         validator: function (value) {
-            $.fn.validatebox.defaults.rules.FmtZeroToTenNum.message = go_i18nLang.Validation.Formatter["FmtZeroToTenNum"]  ;
-            return go_validateClass.FmtZeroToTenNum(value).success;
+            var lo_checkResult = go_validateClass.FmtZeroToTenNum(value);
+            $.fn.validatebox.defaults.rules.FmtZeroToTenNum.message = lo_checkResult.msg;
+            return lo_checkResult.success;
         },
         message: '0到10碼數字'
     },
     //0到10碼數字(數字必須大於等於-1)
     FmtZeroToTenNumAndMinusOne: {
         validator: function (value) {
-            $.fn.validatebox.defaults.rules.FmtZeroToTenNum.message = go_i18nLang.Validation.Formatter["FmtZeroToTenNumAndMinusOne"]  ;
-            return go_validateClass.FmtZeroToTenNumAndMinusOne(value).success;
+            var lo_checkResult = go_validateClass.FmtZeroToTenNumAndMinusOne(value);
+            $.fn.validatebox.defaults.rules.FmtZeroToTenNumAndMinusOne.message = lo_checkResult.msg;
+            return lo_checkResult.success;
         },
         message: '0到10碼數字(數字必須大於等於-1)'
     },
@@ -72,7 +78,7 @@ $.extend($.fn.validatebox.defaults.rules, {
     //大於等於0
     ChkGteZeroNum: {
         validator: function (value) {
-            var lo_checkResult = go_validateClass.ChkGreaterZeroNum(value);
+            var lo_checkResult = go_validateClass.ChkGteZeroNum(value);
             $.fn.validatebox.defaults.rules.ChkGteZeroNum.message = lo_checkResult.msg;
             return lo_checkResult.success;
         },
@@ -81,52 +87,45 @@ $.extend($.fn.validatebox.defaults.rules, {
     //小於等於0
     ChkLteZeroNum: {
         validator: function (value) {
-            $.fn.validatebox.defaults.rules.ChkLteZeroNum.message = go_i18nLang.Validation.Formatter["ChkLteZeroNum"]  ;
-            return go_validateClass.ChkLteZeroNum(value).success;
+            var lo_checkResult = go_validateClass.ChkLteZeroNum(value);
+            $.fn.validatebox.defaults.rules.ChkLteZeroNum.message = lo_checkResult.msg;
+            return lo_checkResult.success;
         },
         message: '請輸入小於等於0的數字'
     },
     //小於0
     ChkLessZeroNum: {
         validator: function (value) {
-            $.fn.validatebox.defaults.rules.ChkLessZeroNum.message = go_i18nLang.Validation.Formatter["ChkLessZeroNum"]  ;
-            return go_validateClass.ChkLessZeroNum(value).success;
+            var lo_checkResult = go_validateClass.ChkLessZeroNum(value);
+            $.fn.validatebox.defaults.rules.ChkLessZeroNum.message = lo_checkResult.msg;
+            return lo_checkResult.success;
         },
         message: '請輸入小於0的數字'
     },
     //時間格式HH:MM
     FmtHHMM : {
         validator: function (value) {
-            var reg = /([0-1][0-9]|2[0-3])\:[0-5][0-9]/;
-            $.fn.validatebox.defaults.rules.FmtHHMM.message = go_i18nLang.Validation.Formatter["FmtHHMM"]  ;
-            return go_validateClass.FmtHHMM(value).success;
+            var lo_checkResult = go_validateClass.FmtHHMM(value);
+            $.fn.validatebox.defaults.rules.FmtHHMM.message = lo_checkResult.msg;
+            return lo_checkResult.success;
         },
         message: '時間格式HH:MM'
     },
     //只能0~100
-    ChkKeepday : {
+    ChkZeroToHundred : {
         validator: function (value) {
-            var reg = /^(([1-9]\d?)|0|100)$/;
-            $.fn.validatebox.defaults.rules.FmtHHMM.message = go_i18nLang.Validation.Formatter["chkKeepday"]  ;
-            return go_validateClass.ChkKeepday(value).success;
-        },
-        message: '只能0~100'
-    },
-    //只能0~100
-    ChkDprat : {
-        validator: function (value) {
-            var reg = /^(([1-9]\d?)|0|100)$/;
-            $.fn.validatebox.defaults.rules.FmtHHMM.message = go_i18nLang.Validation.Formatter["ChkDprat"]  ;
-            return go_validateClass.ChkDprat(value).success;
+            var lo_checkResult = go_validateClass.ChkZeroToHundred(value);
+            $.fn.validatebox.defaults.rules.ChkZeroToHundred.message = lo_checkResult.msg;
+            return lo_checkResult.success;
         },
         message: '只能0~100'
     },
     //只能0~99999
-    ChkViewseq : {
+    ChkZeroToMaxNum : {
         validator: function (value) {
-            var reg = /^(([1-9]\d?){4})$/;
-            $.fn.validatebox.defaults.rules.FmtHHMM.message = go_i18nLang.Validation.Formatter["chkKeepday"]  ;
-            return go_validateClass.ChkViewseq(value).success;
+            var lo_checkResult = go_validateClass.ChkZeroToMaxNum(value);
+            $.fn.validatebox.defaults.rules.ChkZeroToMaxNum.message = lo_checkResult.msg;
+            return lo_checkResult.success;
         },
         message: '只能0~99999'
     }
--- public/js/easyUI/validateClass.js
+++ public/js/easyUI/validateClass.js
@@ -7,10 +7,13 @@ function validateClass() {
     this.ls_msg = go_i18nLang.Validation.Formatter;
 
     //欄位必填
-    this.required = function (value, ui_display_name) {
-        var lb_result = value == "";
-        var ls_msg = sprintf(this.ls_msg.Required, ui_display_name);
-        return {success: !lb_result, msg: ls_msg};
+    this.required = function () {
+        var ls_value = arguments[0];
+        var ls_ui_display_name = arguments[1];
+        var lb_result = ls_value != "";
+        var ls_msg = (arguments.length == 2) ? sprintf(this.ls_msg.Required, ls_ui_display_name) : sprintf(this.ls_msg.Required, "");
+
+        return {success: lb_result, msg: ls_msg};
     };
 
     //資料長度驗證
@@ -24,129 +27,134 @@ function validateClass() {
     };
 
     //四碼英文數字
-    this.FmtFourEngNum = function (value) {
+    this.FmtFourEngNum = function () {
+        var ls_value = arguments[0];
+        var ls_ui_display_name = arguments[1];
         var reg = /^[\w\d]{4}$/;
-        var lb_result = reg.test(value);
-        return {success: lb_result, msg: this.ls_msg.FmtFourEngNum};
+        var lb_result = reg.test(ls_value);
+        var ls_msg = (arguments.length == 2) ? sprintf(this.ls_msg.FmtFourEngNum, ls_ui_display_name) : sprintf(this.ls_msg.FmtFourEngNum, "");
+
+        return {success: lb_result, msg: ls_msg};
     };
 
     //數字文字
-    this.FmtCharacters = function (value) {
+    this.FmtCharacters = function () {
+        var ls_value = arguments[0];
+        var ls_ui_display_name = arguments[1];
         var reg = /[\d\D\w\W]/;
-        var lb_result = reg.test(value);
-        return {success: lb_result, msg: this.ls_msg.FmtCharacters};
+        var lb_result = reg.test(ls_value);
+        var ls_msg = (arguments.length == 2) ? sprintf(this.ls_msg.FmtCharacters, ls_ui_display_name) : sprintf(this.ls_msg.FmtCharacters, "");
+        return {success: lb_result, msg: ls_msg};
     };
 
     //一定要輸入兩碼
-    this.FmtExactlyTwoWord = function (value) {
+    this.FmtExactlyTwoWord = function () {
+        var ls_value = arguments[0];
+        var ls_ui_display_name = arguments[1];
         var reg = /[\w]{2}/;
-        var lb_result = reg.test(value);
-        return {success: lb_result, msg: this.ls_msg.FmtExactlyTwoWord};
+        var lb_result = reg.test(ls_value);
+        var ls_msg = (arguments.length == 2) ? sprintf(this.ls_msg.FmtExactlyTwoWord, ls_ui_display_name) : sprintf(this.ls_msg.FmtExactlyTwoWord, "");
+        return {success: lb_result, msg: ls_msg};
     };
 
     //0到5碼數字
-    this.FmtZeroToFiveNum = function (value) {
+    this.FmtZeroToFiveNum = function () {
+        var ls_value = arguments[0];
+        var ls_ui_display_name = arguments[1];
         var reg = /^\d{0,5}$/;
-        var lb_result = reg.test(value);
-        return {success: lb_result, msg: this.ls_msg.FmtZeroToFiveNum};
+        var lb_result = reg.test(ls_value);
+        var ls_msg = (arguments.length == 2) ? sprintf(this.ls_msg.FmtZeroToFiveNum, ls_ui_display_name) : sprintf(this.ls_msg.FmtZeroToFiveNum, "");
+        return {success: lb_result, msg: ls_msg};
     };
 
     //0到10碼數字
     this.FmtZeroToTenNum = function () {
-        var reg = /^\d{0,10}$/;
         var ls_value = arguments[0];
+        var ls_ui_display_name = arguments[1];
+        var reg = /^\d{0,10}$/;
         var lb_result = reg.test(ls_value);
-        var ls_msg;
+        var ls_msg = (arguments.length == 2) ? sprintf(this.ls_msg.FmtZeroToTenNum, ls_ui_display_name) : sprintf(this.ls_msg.FmtZeroToTenNum, "");
 
-        if (arguments.length == 2) {
-            var ls_ui_display_name = arguments[1];
-            ls_msg = sprintf(this.ls_msg.FmtZeroToTenNum, ls_ui_display_name);
-        }
-        else {
-            ls_msg = sprintf(this.ls_msg.FmtZeroToTenNum, "");
-        }
         return {success: lb_result, msg: ls_msg};
 
     };
 
     //0到10碼數字(數字必須大於等於-1)
-    this.FmtZeroToTenNumAndMinusOne = function (value) {
+    this.FmtZeroToTenNumAndMinusOne = function () {
+        var ls_value = arguments[0];
+        var ls_ui_display_name = arguments[1];
         var reg = /-?^\d{0,10}$/;
-        var lb_result = reg.test(value);
-        return {success: lb_result, msg: this.ls_msg.FmtZeroToTenNumAndMinusOne};
+        var lb_result = reg.test(ls_value) && (Number(ls_value) >= -1 && ls_value != "-0");
+        var ls_msg = (arguments.length == 2) ? sprintf(this.ls_msg.FmtZeroToTenNumAndMinusOne, ls_ui_display_name) : sprintf(this.ls_msg.FmtZeroToTenNumAndMinusOne, "");
+        return {success: lb_result, msg: ls_msg};
     };
 
     //大於0
     this.ChkGreaterZeroNum = function () {
         var ls_value = arguments[0];
+        var ls_ui_display_name = arguments[1];
         var lb_result = Number(ls_value) > 0;
-        var ls_msg;
+        var ls_msg = (arguments.length == 2) ? sprintf(this.ls_msg.ChkGreaterZeroNum, ls_ui_display_name) : sprintf(this.ls_msg.ChkGreaterZeroNum, "");
 
-        console.log(arguments.length);
-        if (arguments.length == 2) {
-            var ls_ui_display_name = arguments[1];
-            ls_msg = sprintf(this.ls_msg.ChkGreaterZeroNum, ls_ui_display_name);
-        }
-        else {
-            ls_msg = sprintf(this.ls_msg.ChkGreaterZeroNum, "");
-            console.log(ls_msg);
-        }
         return {success: lb_result, msg: ls_msg};
     };
 
     //大於等於0
     this.ChkGteZeroNum = function () {
         var ls_value = arguments[0];
+        var ls_ui_display_name = arguments[1];
         var lb_result = Number(ls_value) >= 0;
-        var ls_msg;
+        var ls_msg = (arguments.length == 2) ? sprintf(this.ls_msg.ChkGteZeroNum, ls_ui_display_name) : sprintf(this.ls_msg.ChkGteZeroNum, "");
 
-        if (arguments.length == 2) {
-            var ls_ui_display_name = arguments[1];
-            ls_msg = sprintf(this.ls_msg.ChkGteZeroNum, ls_ui_display_name);
-        }
-        else {
-            ls_msg = sprintf(this.ls_msg.ChkGteZeroNum, "");
-        }
         return {success: lb_result, msg: ls_msg};
-
     };
 
-    //大於等於0
-    // this.ChkGteZeroNum = function (value) {
-    //     var lb_result = Number(value) >= 0;
-    //     var ls_msg = sprintf(this.ls_msg.ChkGteZeroNum, "");
-    //     return {success: lb_result, msg: ls_msg};
-    // };
-
     //小於等於0
-    this.ChkLteZeroNum = function (value) {
-        var lb_result = Number(value) <= 0;
+    this.ChkLteZeroNum = function () {
+        var ls_value = arguments[0];
+        var ls_ui_display_name = arguments[1];
+        var lb_result = Number(ls_value) <= 0;
+        var ls_msg = (arguments.length == 2) ? sprintf(this.ls_msg.ChkLteZeroNum, ls_ui_display_name) : sprintf(this.ls_msg.ChkLteZeroNum, "");
         return {success: lb_result, msg: this.ls_msg.ChkLteZeroNum};
     };
 
     //小於0
-    this.ChkLessZeroNum = function (value) {
-        var lb_result = Number(value) < 0;
-        return {success: lb_result, msg: this.ls_msg.ChkLteZeroNum};
+    this.ChkLessZeroNum = function () {
+        var ls_value = arguments[0];
+        var ls_ui_display_name = arguments[1];
+        var lb_result = Number(ls_value) < 0;
+        var ls_msg = (arguments.length == 2) ? sprintf(this.ls_msg.ChkLessZeroNum, ls_ui_display_name) : sprintf(this.ls_msg.ChkLessZeroNum, "");
+        return {success: lb_result, msg: ls_msg};
     };
 
     //時間格式HH:MM
-    this.FmtHHMM = function (value) {
-        var lb_result = Number(value) < 0;
-        return {success: lb_result, msg: this.ls_msg.ChkLteZeroNum};
+    this.FmtHHMM = function () {
+        var ls_value = arguments[0];
+        var ls_ui_display_name = arguments[1];
+        var reg = /([0-1][0-9]|2[0-3])\:[0-5][0-9]/;
+        var lb_result = reg.test(ls_value);
+        var ls_msg = (arguments.length == 2) ? sprintf(this.ls_msg.FmtHHMM, ls_ui_display_name) : sprintf(this.ls_msg.FmtHHMM, "");
+        return {success: lb_result, msg: ls_msg};
     };
 
     //只能0~100
-    this.ChkKeepday = function (value) {
+    this.ChkZeroToHundred = function () {
+        var ls_value = arguments[0];
+        var ls_ui_display_name = arguments[1];
         var reg = /^(([1-9]\d?)|0|100)$/;
-        var lb_result = reg.test(value);
-        return {success: lb_result, msg: this.ls_msg.ChkKeepday};
+        var lb_result = reg.test(ls_value);
+        var ls_msg = (arguments.length == 2) ? sprintf(this.ls_msg.ChkZeroToHundred, ls_ui_display_name) : sprintf(this.ls_msg.ChkZeroToHundred, "");
+        return {success: lb_result, msg: ls_msg};
     };
 
     //只能0~99999
-    this.ChkViewseq = function (value) {
-        var reg = /^(([1-9]\d?){4})$/;
-        var lb_result = reg.test(value);
-        return {success: lb_result, msg: this.ls_msg.ChkViewseq};
+    this.ChkZeroToMaxNum = function () {
+        var ls_value = arguments[0];
+        var ls_ui_display_name = arguments[1];
+        var reg = /\b([0-9]{1,5}\d)\b/;
+        var lb_result = reg.test(ls_value);
+        console.log(lb_result, ls_value);
+        var ls_msg = (arguments.length == 2) ? sprintf(this.ls_msg.ChkZeroToMaxNum, ls_ui_display_name) : sprintf(this.ls_msg.ChkZeroToMaxNum, "");
+        return {success: lb_result, msg: ls_msg};
     };
 }
