492dc0eb6e048ba167a627e4053b4488f64d241f
a16010 <a16010@athena.com.tw>
2017-11-13 02:49:26 +0800

    頁面顯示資料完成

--- public/js/reserveBanquet/RS00202010_setup.js
+++ public/js/reserveBanquet/RS00202010_setup.js
@@ -2,15 +2,189 @@ var RS00202010VM = new Vue({
     el: "#RS00202010Main",
     data: {
         defaultDate: new Date(),
-        searchDate: ""
+        searchDate: "",
+        pageOneData: {
+            time_range: ["09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00",
+                "19:00", "20:00", "21:00", "22:00", "23:00", "00:00", "01:00", "02:00"],
+            rowData:[
+                {
+                    tr_class: "no-cursor-tr h23-tr",
+                    datatype: "RSPT",
+                    name: "蜻蜓餐廳",
+                    rowspan: 0,
+                    banquet_dt: [
+                        {
+                            name: "BREAKFAST2",
+                            beg_tim: "09:00",
+                            end_tim: "10:30",
+                            colspan: 3,
+                            datatype: "MTIME",
+                            order_sta: ""
+                        },
+                        {
+                            name: "LUNCH2",
+                            beg_tim: "09:00",
+                            end_tim: "10:30",
+                            colspan: 8,
+                            datatype: "MTIME",
+                            order_sta: ""
+                        },
+                        {
+                            name: "SNACK2",
+                            beg_tim: "14:30",
+                            end_tim: "17:30",
+                            colspan: 6,
+                            datatype: "MTIME",
+                            order_sta: ""
+                        },
+                        {
+                            name: "DINNER",
+                            beg_tim: "17:30",
+                            end_tim: "23:00",
+                            colspan: 13,
+                            datatype: "MTIME",
+                            order_sta: ""
+                        },
+                        {
+                            name: "",
+                            beg_tim: "00:00",
+                            end_tim: "02:00",
+                            colspan: 6,
+                            datatype: "MTIME",
+                            order_sta: ""
+                        }
+                    ]
+                },
+                {
+                    tr_class: "",
+                    datatype: "PLACE",
+                    name: "宴蜓區 (5)",
+                    rowspan: 2,
+                    banquet_dt: [
+                        {
+                            name: "test1",
+                            beg_tim: "09:00",
+                            end_tim: "00:00",
+                            colspan: 32,
+                            datatype: "Reserve",
+                            order_sta: "N"
+                        },
+                        {
+                            name: "test2",
+                            beg_tim: "09:00",
+                            end_tim: "00:00",
+                            colspan: 2,
+                            datatype: "Reserve",
+                            order_sta: "W"
+                        },
+                        {
+                            datatype: "",
+                            repeat: 2
+                        }
+                    ]
+                },
+                {
+                    tr_class: "",
+                    datatype: "PLACE",
+                    name: "宴蜓區 (5)",
+                    rowspan: -1,
+                    banquet_dt: [
+                        {
+                            repeat: 4,
+                            datatype: ""
+                        },
+                        {
+                            name: "(詢)test123_1",
+                            beg_tim: "09:00",
+                            end_tim: "00:00",
+                            colspan: 28,
+                            datatype: "Reserve",
+                            order_sta: "Q"
+                        },
+                        {
+                            name: "test2",
+                            beg_tim: "09:00",
+                            end_tim: "00:00",
+                            colspan: 2,
+                            datatype: "Reserve",
+                            order_sta: "W"
+                        },
+                        {
+                            datatype: "",
+                            repeat: 2
+                        }
+                    ]
+                },
+                {
+                    tr_class: "no-cursor-tr h23-tr",
+                    datatype: "RSPT",
+                    name: "雨林餐廳",
+                    rowspan: 0,
+                    banquet_dt: [
+                        {
+                            name: "BREAKFAST1",
+                            beg_tim: "09:00",
+                            end_tim: "10:30",
+                            colspan: 3,
+                            datatype: "MTIME"
+                        },
+                        {
+                            name: "LUNCH2",
+                            beg_tim: "09:00",
+                            end_tim: "10:30",
+                            colspan: 6,
+                            datatype: "MTIME"
+                        },
+                        {
+                            name: "",
+                            beg_tim: "00:00",
+                            end_tim: "02:00",
+                            colspan: 7,
+                            datatype: "MTIME"
+                        },
+                        {
+                            name: "DINNER",
+                            beg_tim: "14:30",
+                            end_tim: "17:30",
+                            colspan: 8,
+                            datatype: "MTIME"
+                        },
+                        {
+                            name: "",
+                            beg_tim: "14:30",
+                            end_tim: "17:30",
+                            colspan: 2,
+                            datatype: "MTIME"
+                        },
+                        {
+                            name: "MIDNIGHT",
+                            beg_tim: "00:00",
+                            end_tim: "02:00",
+                            colspan: 8,
+                            datatype: "MTIME"
+                        },
+                        {
+                            name: "",
+                            beg_tim: "14:30",
+                            end_tim: "17:30",
+                            colspan: 2,
+                            datatype: "MTIME"
+                        }
+                    ]
+                }
+            ]
+        }
     },
     mounted: function () {
+        var self = this;
         //啟用fixTable
         $("#gs-fixTable").tableHeadFixer({"left": 1});
         $("table.treeControl").agikiTreeTable({persist: true, persistStoreName: "files"});
         $.post("/reserveBanquet/qryPageOneData", function (result) {
             if (result.success) {
-                console.log(result.pageOneData);
+                self.pageOneData.time_range = result.pageOneData.time_range;
+                self.pageOneData.rowData = result.pageOneData.rowData;
+                console.log(result.pageOneData.rowData);
             }
         });
     }
--- services/reserveBanquetService.js
+++ services/reserveBanquetService.js
@@ -24,10 +24,11 @@ exports.qryPageOneData = function (postData, session, callback) {
         qryBanquetData,     // 查訂席平面圖資料
         qryBanquetSta       // 查訂席場地狀態
     ], function (err, result) {
-        var la_banquetData = result[0];
-        var la_banquetSta = result[1];
-        convertDataToDisplay(la_banquetData, la_banquetSta);
-        lo_result.defaultValues = la_banquetData;
+        let la_banquetData = result[0];
+        let la_banquetSta = result[1];
+        let lo_banquetData = convertDataToDisplay(la_banquetData, la_banquetSta);
+        console.log(lo_banquetData);
+        lo_result.defaultValues = lo_banquetData;
         callback(lo_error, lo_result);
     });
 
@@ -49,35 +50,235 @@ exports.qryPageOneData = function (postData, session, callback) {
 //[RS00202010] 將資料轉換為顯示用格式
 function convertDataToDisplay(la_data, la_sta) {
     let lo_resvBanquetData = new ResvBanquetData(la_data, la_sta);
-    lo_resvBanquetData.convertExec();
-    return la_data;
+    let lo_converData = lo_resvBanquetData.convertExec();
+    return lo_converData;
 }
 
 class ResvBanquetData {
-    constructor(la_data, la_sta) {
-        this.la_time_range = [];
+    constructor(la_data, la_order) {
         this.ls_beg_hour = _.findWhere(la_data, {datatype: "BEG_HOUR"}).beg_hour;
         this.ls_end_hour = _.findWhere(la_data, {datatype: "END_HOUR"}).end_hour;
         this.la_rspt = _.where(la_data, {datatype: "RSPT"});
         this.la_place = _.where(la_data, {datatype: "PLACE"});
-        this.la_mtime = _.where(la_data, {datatype: "MTIME"});
+        this.la_mtim = _.where(la_data, {datatype: "MTIME"});
+        this.la_order = la_order;
     }
 
     convertExec() {
-        let la_rtnData = {};
-        this.getTimeRange();
+        let la_rtnData = {
+            time_range: this.getTimeRange(),
+            rowData: this.genRowData()
+        };
+        return la_rtnData;
     }
 
     getTimeRange() {
+        let la_time_range = [];
         let ln_time_range;
+
         this.ls_beg_hour = moment(this.ls_beg_hour, "HH");
         this.ls_end_hour = moment(this.ls_end_hour, "HH");
         ln_time_range = this.ls_end_hour.diff(this.ls_beg_hour, "hour");
         for (var min = 0; min <= ln_time_range; min++) {
-            this.la_time_range.push(this.ls_beg_hour.clone().add(min, "hour").format("HH:mm"));
+            la_time_range.push(this.ls_beg_hour.clone().add(min, "hour").format("HH:mm"));
         }
-        console.log(this.la_time_range);
+        return la_time_range;
+    }
 
+    genRowData() {
+        let self = this;
+        let la_rowData = [];
+        let lo_rowData = {};
+
+        // 餐廳
+        _.each(this.la_rspt, function (lo_rspt) {
+            lo_rowData = {
+                tr_class: "no-cursor-tr h23-tr",
+                datatype: lo_rspt.datatype,
+                name: lo_rspt.rspt_nam,
+                banquet_dt: self.genBanquet_dt(lo_rspt.datatype, lo_rspt.rspt_cod),
+                rowspan: 0
+            };
+            la_rowData.push(lo_rowData);
+
+            // 地區
+            let la_place = _.where(self.la_place, {rspt_cod: lo_rspt.rspt_cod});
+            _.each(la_place, function (lo_place) {
+                lo_rowData = {
+                    tr_class: "",
+                    datatype: "PLACE",
+                    name: lo_place.place_nam,
+                    banquet_dt: self.genBanquet_dt("Reserve", lo_place.place_cod),
+                    rowspan: 0
+                };
+                la_rowData.push(lo_rowData);
+            });
+        });
+        return la_rowData;
+    }
+
+    genBanquet_dt(datatype, parent_cod) {
+        let self = this;
+        let la_banquet_dt = [];
+        let lo_banquet_dt = {};
+        let ln_colspan;
+
+        // 餐期
+        if (datatype == "RSPT") {
+            let la_mtim = _.where(this.la_mtim, {rspt_cod: parent_cod});
+            _.each(la_mtim, function (lo_mtim, index) {
+                let lo_begin_tim = moment(lo_mtim.begin_tim, "HHmm");
+                let lo_end_tim = moment(lo_mtim.end_tim, "HHmm");
+
+                if (index == 0) {
+                    if (self.ls_beg_hour.format("HH:mm") != lo_begin_tim.format("HH:mm")) {
+                        ln_colspan = self.calcColSpan(self.ls_beg_hour, lo_begin_tim);
+                        lo_banquet_dt = {
+                            name: "",
+                            beg_tim: self.ls_beg_hour.format("HH:mm"),
+                            end_tim: lo_begin_tim.format("HH:mm"),
+                            colspan: ln_colspan,
+                            datatype: lo_mtim.datatype
+                        };
+                        if (ln_colspan != 0) {
+                            la_banquet_dt.push(lo_banquet_dt);
+                        }
+                    }
+                }
+                else if (la_banquet_dt[la_banquet_dt.length - 1].end_tim != lo_begin_tim.format("HH:mm")) {
+                    ln_colspan = self.calcColSpan(moment(la_banquet_dt[la_banquet_dt.length - 1].end_tim, "HH:mm"), lo_begin_tim);
+                    lo_banquet_dt = {
+                        name: "",
+                        beg_tim: moment(la_banquet_dt[la_banquet_dt.length - 1].end_tim, "HH:mm").format("HH:mm"),
+                        end_tim: lo_begin_tim.format("HH:mm"),
+                        colspan: ln_colspan,
+                        datatype: lo_mtim.datatype
+                    };
+                    if (ln_colspan != 0) {
+                        la_banquet_dt.push(lo_banquet_dt);
+                    }
+                }
+
+                ln_colspan = self.calcColSpan(lo_begin_tim, lo_end_tim);
+                lo_banquet_dt = {
+                    name: lo_mtim.time_nam,
+                    beg_tim: lo_begin_tim.format("HH:mm"),
+                    end_tim: lo_end_tim.format("HH:mm"),
+                    colspan: ln_colspan,
+                    datatype: lo_mtim.datatype
+                };
+                la_banquet_dt.push(lo_banquet_dt);
+            });
+
+            let lo_last = _.last(la_banquet_dt);
+            if (!_.isUndefined(lo_last) && lo_last.end_tim != self.ls_end_hour.format("HH:mm")) {
+                ln_colspan = self.calcColSpan(moment(lo_last.end_tim, "HH:mm"), self.ls_end_hour.clone().add("1", "hours"));
+                lo_banquet_dt = {
+                    name: "",
+                    beg_tim: moment(lo_last.end_tim, "HH:mm").format("HH:mm"),
+                    end_tim: self.ls_end_hour.format("HH:mm"),
+                    colspan: ln_colspan,
+                    datatype: "MTIME"
+                };
+                la_banquet_dt.push(lo_banquet_dt);
+            }
+
+            if (la_banquet_dt.length == 0) {
+                ln_colspan = self.calcColSpan(self.ls_beg_hour, self.ls_end_hour.clone().add("1", "hours"));
+                lo_banquet_dt = {
+                    name: "",
+                    beg_tim: self.ls_beg_hour.format("HH:mm"),
+                    end_tim: self.ls_end_hour.format("HH:mm"),
+                    colspan: ln_colspan,
+                    datatype: "MTIME"
+                };
+                la_banquet_dt.push(lo_banquet_dt);
+            }
+        }
+        else {
+            let la_order = _.where(this.la_order, {place_cod: parent_cod});
+            _.each(la_order, function (lo_order, index) {
+                let lo_begin_tim = moment(lo_order.begin_tim, "HHmm");
+                let lo_end_tim = moment(lo_order.end_tim, "HHmm");
+                if (index == 0) {
+                    if (self.ls_beg_hour.format("HH:mm") != lo_begin_tim.format("HH:mm")) {
+                        ln_colspan = self.calcColSpan(self.ls_beg_hour, lo_begin_tim);
+                        lo_banquet_dt = {
+                            beg_tim: self.ls_beg_hour.format("HH:mm"),
+                            end_tim: lo_begin_tim.format("HH:mm"),
+                            datatype: "",
+                            repeat: ln_colspan
+                        };
+                        if (ln_colspan != 0) {
+                            la_banquet_dt.push(lo_banquet_dt);
+                        }
+                    }
+                }
+                else if (la_banquet_dt[la_banquet_dt.length - 1].end_tim != lo_begin_tim.format("HH:mm")) {
+                    ln_colspan = self.calcColSpan(moment(la_banquet_dt[la_banquet_dt.length - 1].end_tim, "HH:mm"), lo_begin_tim);
+                    lo_banquet_dt = {
+                        beg_tim: moment(la_banquet_dt[la_banquet_dt.length - 1].end_tim, "HH:mm").format("HH:mm"),
+                        end_tim: lo_begin_tim.format("HH:mm"),
+                        repeat: ln_colspan,
+                        datatype: ""
+                    };
+                    if (ln_colspan != 0) {
+                        la_banquet_dt.push(lo_banquet_dt);
+                    }
+                }
+
+                let order_nam;
+                ln_colspan = self.calcColSpan(lo_begin_tim, lo_end_tim);
+                if (lo_order.order_sta == "N") {
+                    order_nam = lo_order.title_nam;
+                }
+                else if (lo_order.order_sta == "W") {
+                    order_nam = "(等" + lo_order.wait_seq + ") " + lo_order.title_nam;
+                }
+                else {
+                    order_nam = "(詢) " + lo_order.title_nam;
+                }
+                lo_banquet_dt = {
+                    name: order_nam,
+                    beg_tim: lo_begin_tim.format("HH:mm"),
+                    end_tim: lo_end_tim.format("HH:mm"),
+                    colspan: ln_colspan,
+                    datatype: "Reserve",
+                    order_sta: lo_order.order_sta
+                };
+                la_banquet_dt.push(lo_banquet_dt);
+            });
+
+            let lo_last = _.last(la_banquet_dt);
+            if (!_.isUndefined(lo_last) && lo_last.end_tim != self.ls_end_hour.format("HH:mm")) {
+                ln_colspan = self.calcColSpan(moment(lo_last.end_tim, "HH:mm"), self.ls_end_hour.clone().add("1", "hours"));
+                lo_banquet_dt = {
+                    beg_tim: moment(lo_last.end_tim, "HH:mm").format("HH:mm"),
+                    end_tim: self.ls_end_hour.format("HH:mm"),
+                    repeat: ln_colspan,
+                    datatype: ""
+                };
+                la_banquet_dt.push(lo_banquet_dt);
+            }
+
+            if (la_banquet_dt.length == 0) {
+                ln_colspan = self.calcColSpan(self.ls_beg_hour, self.ls_end_hour.clone().add("1", "hours"));
+                lo_banquet_dt = {
+                    beg_tim: self.ls_beg_hour.format("HH:mm"),
+                    end_tim: self.ls_end_hour.format("HH:mm"),
+                    repeat: ln_colspan,
+                    datatype: ""
+                };
+                la_banquet_dt.push(lo_banquet_dt);
+            }
+        }
+        return la_banquet_dt;
+    }
+
+    calcColSpan(lo_begin_tim, lo_end_tim) {
+        let ln_diffMin = lo_end_tim.diff(lo_begin_tim, "minutes");
+        let ln_colspan = Math.round(ln_diffMin / 30);
+        return ln_colspan;
     }
 }
 
