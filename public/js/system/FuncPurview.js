e61435325a7049cee243358e7897d9821bff727c
a14017 <chang.maz@athena.com.tw>
2017-11-14 19:54:46 +0800

    按鈕權限控制

--- public/js/system/FuncPurview.js
+++ public/js/system/FuncPurview.js
@@ -1,9 +1,9 @@
 /**
  * 按鈕權限控制
- * @param prg_id
+ * @param prg_id : 作業編號
  * @constructor
- *
- * 按鈕在class 綁上  purview_btn_[func_id] (ex: purview_btn_0200)
+ * 按鈕Class 綁上 purview_btn
+ * 按鈕在屬性data-purview_func_id  [作業編號+func_id] (ex: PMS081000010200)
  */
 function FuncPurview(prg_id) {
     if (prg_id == undefined || prg_id == "") {
@@ -11,17 +11,19 @@ function FuncPurview(prg_id) {
     }
     this.funcPurvs = [];
     $.post("/api/getUserFuncPurviewByProID", {prg_id: prg_id}, function (result) {
-        FuncPurview.funcPurvs = result.funcPurvs;
-        $(".btn").each(function () {
-            if ($(this).attr("class").indexOf("purview_btn_") > -1) {
-                $(this).attr("disabled", true);
+        FuncPurview.funcPurvs = result.funcPurvs.slice(0, 1);
+        //判斷按鈕是否有權限使用
+        $(".purview_btn").each(function () {
+            var purview_func_id = $(this).data("purview_func_id");
+            var purIdx = _.findIndex(FuncPurview.funcPurvs, function (func) {
+                return prg_id + func.func_id == purview_func_id;
+            });
+            if (purIdx == -1) {
+                $("[data-purview_func_id='" + purview_func_id + "']").attr("disabled", true);
             }
         });
-        //判斷按鈕是否有權限使用
-        FuncPurview.funcPurvs.forEach(function (func) {
-            $(".purview_btn_" + func.func_id).attr("disabled", false);
-        });
     });
+
 
 }
 
