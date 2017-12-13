307a3dc454338b0f9c97db5f621b132c51e66750
a16010 <lai.kaiyue@athena.com.tw>
2017-12-13 19:18:24 +0800

    修改人員樹狀功能

--- public/src/admin/permission/components/authStaffComp.vue
+++ public/src/admin/permission/components/authStaffComp.vue
@@ -1,46 +1,97 @@
 <template>
     <div>
         <p class="topTitle">人員權限</p>
-        <!--<div class="easyui-panel tree-body" style="width: 100%;">-->
-            <!--<ul id="staffTree" class="easyui-tree">{{ staffTreeData }}</ul>-->
-        <!--</div>-->
-        <template v-for="staff in allStaff">
-            <input type="checkbox" :value="staff.user_id" v-model="staffOfRole">{{staff.user_id}}<br>
-        </template>
         {{staffOfRole}}
+        <div class="easyui-panel tree-body" style="width: 100%;">
+            <ul id="permissionAccountTree" class="easyui-tree"></ul>
+        </div>
 
     </div>
 </template>
 
 <script>
-    import {mapGetters} from 'vuex';
+    import {mapState, mapGetters} from 'vuex';
 
     export default {
         name: "auth-staff-comp",
+        data() {
+            return {
+                treeIns: null
+            }
+        },
         mounted() {
-            // this.initTree();
+            this.qryCompGrpList();
         },
         computed: {
-            allStaff(){
-                return this.$store.state.allStaff;
-            },
             staffOfRole: {
-                get(){
+                get() {
+                    this.updateAccountChkedTree(this.$store.state.staffOfRole);
                     return this.$store.state.staffOfRole;
                 },
-                set(value){
-                    console.log(value);
-                    // this.$store.commit("updateStaffOfRole", value);
+                set(value) {
+                    this.$store.commit("updateStaffOfRole", value);
                 }
             }
         },
         methods: {
-            // initTree(){
-            //     let self = this;
-            //     $("#staffTree").tree({
-            //         data: self.staffTreeData
-            //     });
-            // }
+            qryCompGrpList() {
+                let self = this;
+                this.$store.dispatch("qryCompGrp").then((la_compGrpList4Tree) => {
+                    self.createAccountTree(la_compGrpList4Tree);
+                })
+            },
+            //創立一棵Tree
+            createAccountTree: function (la_compGrpList4Tree) {
+                $('#permissionAccountTree').jstree({
+                    "core": {
+                        "animation": 0,
+                        "themes": {"stripes": true},
+                        "multiple": false,                  //不可多選
+                        "check_callback": true,
+                        "data": la_compGrpList4Tree
+                    },
+                    "types": {
+                        "#": {
+                            "icon": "glyphicon glyphicon-file",
+                            "max_children": 1,
+                            "max_depth": 4,
+                            "valid_children": ["root"]
+                        },
+                        "root": {
+                            "icon": "glyphicon glyphicon-file",
+                            "valid_children": ["default"]
+                        },
+                        "default": {
+                            "valid_children": ["default", "file"]
+                        },
+                        "file": {
+                            "icon": "glyphicon glyphicon-file",
+                            "valid_children": []
+                        }
+                    },
+                    "checkbox": {
+                        "keep_selected_style": true,
+                        "whole_node": false,
+                        "tie_selection": false               // 選取時，false只會選到父節點，不會選到子結點
+                    },
+                    "plugins": ["search", "state", "types", "wholerow", "checkbox"]
+                });
+                this.treeIns = $("#permissionAccountTree").jstree(true);
+                // setTimeout(function () {
+                //     self.treeIns.uncheck_all();
+                //     if (self.allRoles.length > 0) {
+                //         self.role_id = self.allRoles[0].role_id;
+                //     }
+                // }, 100);
+
+            },
+            //更新目標角色對應的帳號標示在Tree上
+            updateAccountChkedTree: function () {
+                this.treeIns.uncheck_all();
+                _.each(this.ga_roleOfAccount, function (account) {
+                    PermissionVM.treeIns.check_node("#" + account.user_id);
+                });
+            },
         }
     }
 </script>
--- public/src/admin/permission/store.js
+++ public/src/admin/permission/store.js
@@ -9,7 +9,9 @@ Vue.use(Vuex);
 const state = {
     allRoles: [],
     selRole: "",
-    allStaff: []
+    staffOfRole: [],
+    compGrpList: [],
+    compGrpList4Tree: []
 }
 
 // mutations are operations that actually mutates the state.
@@ -24,12 +26,26 @@ const mutations = {
     setSelRole(state, selRole) {
         state.selRole = selRole;
     },
-    setAllStaff(state, treeData) {
-        state.allStaff = treeData;
+    setCompGrpList(state, la_compGrpList) {
+        state.compGrpList = la_compGrpList;
     },
-    updateStaffOfRole(state, la_staffOfRole){
-        state.allStaff = la_staffOfRole;
+    pushCompGrpList4Tree(state, lo_compGrpList) {
+        state.compGrpList4Tree.push(lo_compGrpList);
+    },
+    setStaffOfRole(state, la_staffOfRole) {
+        state.staffOfRole = la_staffOfRole;
+    },
+    updateStaffOfRole(state, ls_user_id) {
+        console.log(ls_user_id);
+        let userExist = _.findIndex(state.staffOfRole, {user_id: ls_user_id});
+        if (userExist == -1) {
+            state.staffOfRole.push(ls_user_id);
+        }
+        else {
+            state.staffOfRole.splice(userExist, 0);
+        }
     }
+
 }
 
 // actions are functions that cause side effects and can involve
@@ -49,28 +65,55 @@ const actions = {
         dispatch("qryRoleOfAccounts", role_id);
     },
     //取得角色對應之全部帳號
-    qryRoleOfAccounts({commit}, role_id) {
-        $.post("/api/getRoleOfAccounts", {role_id: role_id}, function (result) {
-            commit("setAllStaff", result.accounts);
+    async qryRoleOfAccounts({commit}, role_id) {
+        await $.post("/api/getRoleOfAccounts", {role_id: role_id}).then((result) => {
+            commit("setStaffOfRole", result.accounts);
         });
+    },
+    //撈取公司組別
+    async qryCompGrp({dispatch, commit, state}) {
+        await $.post("/api/getCompGrp").then((result) => {
+            commit("setCompGrpList", result.compGrpList);
+            dispatch("combineCompGrpTree", result.compGrpList);
+        });
+        return state.compGrpList4Tree;
+    },
+
+    //組合tree的格式給tree
+    combineCompGrpTree({commit}, la_compGrpList) {
+        let lo_treeData = {
+            id: la_compGrpList[0].cmp_id,
+            parent: "#",
+            text: la_compGrpList[0].cmp_name
+        };
+        commit("pushCompGrpList4Tree", lo_treeData);
+        _.each(_.groupBy(la_compGrpList, "grp_name"), function (la_accounts, grp_nam) {
+
+            lo_treeData = {
+                id: la_accounts[0].grp_id,
+                parent: la_accounts[0].cmp_id,
+                text: la_accounts[0].grp_name
+            }
+            commit("pushCompGrpList4Tree", lo_treeData);
+
+            _.each(la_accounts, function (account) {
+                lo_treeData = {
+                    id: account.usr_id,
+                    parent: account.grp_id,
+                    text: account.usr_cname + "(" + account.usr_id + ")"
+                }
+                commit("pushCompGrpList4Tree", lo_treeData);
+            });
+        });
+
+
     }
 }
 
 // getters are functions
 const getters = {
     allRoles: state => state.allRoles,
-    selRole: state => state.selRole,
-    staffOfRole: state => {
-        let staffOfRole = [];
-
-        _.each(state.allStaff, function (eachStaff) {
-            if (eachStaff.role_id == state.selRole) {
-                staffOfRole.push(eachStaff.user_id);
-            }
-        });
-
-        return staffOfRole;
-    }
+    selRole: state => state.selRole
 }
 
 // A Vuex instance is created by combining the state, mutations, actions,
