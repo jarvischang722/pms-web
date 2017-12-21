ca26b9bd0a9d47df270e1ee5dd7a1c5f98bd088b
a16009 <mike1211>
2017-12-21 10:14:22 +0800

    [RS0W212010 訂席] middlewear修改

--- routes/reserveBanquet.js
+++ routes/reserveBanquet.js
@@ -6,27 +6,26 @@ var authMW = require("../middlewares/authMiddleware");
 var sysMW = require("../middlewares/systemMiddleware");
 var i18nMW = require("../middlewares/i18nMiddleware");
 var middles = [i18nMW, authMW, sysMW];
-var apiMiddles = [authMW];
 
 module.exports = function(app, passport){
 
     //reserveBanquet格萊天漾訂席頁
     app.get("/rs/RS0W212010", middles, resvbanquetCtrl.getRS0W212010);
 
-    app.post("/reserveBanquet/qryPageOneData", apiMiddles, resvbanquetCtrl.qryPageOneData);
+    app.post("/reserveBanquet/qryPageOneData", middles, resvbanquetCtrl.qryPageOneData);
 
-    app.post("/reserveBanquet/qryPageTwoData", apiMiddles, resvbanquetCtrl.qryPageTwoData);
+    app.post("/reserveBanquet/qryPageTwoData", middles, resvbanquetCtrl.qryPageTwoData);
 
-    app.post("/reserveBanquet/qrySystemParam", apiMiddles, resvbanquetCtrl.qrySystemParam);
+    app.post("/reserveBanquet/qrySystemParam", middles, resvbanquetCtrl.qrySystemParam);
 
-    app.post("/reserveBanquet/chk_use_typ", apiMiddles, resvbanquetCtrl.chk_use_typ);
+    app.post("/reserveBanquet/chk_use_typ", middles, resvbanquetCtrl.chk_use_typ);
 
-    app.post("/reserveBanquet/def_proc_sta", apiMiddles, resvbanquetCtrl.def_proc_sta);
+    app.post("/reserveBanquet/def_proc_sta", middles, resvbanquetCtrl.def_proc_sta);
 
-    app.post("/reserveBanquet/qry_bqcust_mn", apiMiddles, resvbanquetCtrl.qry_bqcust_mn);
+    app.post("/reserveBanquet/qry_bqcust_mn", middles, resvbanquetCtrl.qry_bqcust_mn);
 
-    app.post("/reserveBanquet/chgOrderStaAPI", apiMiddles, resvbanquetCtrl.chgOrderStaAPI);
+    app.post("/reserveBanquet/chgOrderStaAPI", middles, resvbanquetCtrl.chgOrderStaAPI);
 
-    app.post("/reserveBanquet/getPlaceUnitAmt", apiMiddles, resvbanquetCtrl.getPlaceUnitAmt);
+    app.post("/reserveBanquet/getPlaceUnitAmt", middles, resvbanquetCtrl.getPlaceUnitAmt);
 
 };
