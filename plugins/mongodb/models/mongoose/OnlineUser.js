5a2f0ee81c66a8799fdebeeb4386d9f50bcb9120
a17017 <pohao.wang@athena.com.tw>
2018-01-03 17:09:27 +0800

    [人數控管] -修改 取得oracle人數資料

--- plugins/mongodb/models/mongoose/OnlineUser.js
+++ plugins/mongodb/models/mongoose/OnlineUser.js
@@ -6,13 +6,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 let OnlineUserSchema = new Schema({
     athena_id: {type: Number, index: true, required: true},  //集團編號
-    comp_cod: {type: String, trim: true, index: true, required: true},  //公司編號
-    hotel_cod: {type: String, trim: true, index: true, required: true},  //館別編號
+    comp_cod: {type: String, trim: true},  //公司編號
+    hotel_cod: {type: String, trim: true},  //館別編號
     onlineUserSession: {type: [String]},   　 //在線使用者
     availUserNum: {type: Number},    //可使用的人數
     lastUpdTime: {type: Date}       //更新時間
 }, {collection: "OnlineUser"});
 
-OnlineUserSchema.index({athena_id: 1, company_cod: 1, hotel_cod: 1}, {unique: true});
+OnlineUserSchema.index({athena_id: 1}, {unique: true});
 
 mongoose.model("OnlineUser", OnlineUserSchema);
--- services/AuthService.js
+++ services/AuthService.js
@@ -5,6 +5,8 @@
 var queryAgent = require('../plugins/kplug-oracle/QueryAgent');
 var mongoAgent = require("../plugins/mongodb");
 var async = require("async");
+var _ = require('underscore');
+var moment = require('moment');
 /**
  * 驗證登入
  */
@@ -41,126 +43,101 @@ exports.doAuthAccount = function (authData, callback) {
                     user.fun_hotel_name = hotels.length > 0 ? hotels[0].hotel_nam.trim() : "";
                     user.athena_id = hotels.length > 0 ? hotels[0].athena_id : "";
                     //取得人數上限
-                    /**
-                     * 測試
-                     */
-                    // user.onlineUserBy = {};
-                    // user.onlineUserBy.athena_id = user.athena_id;
-                    // user.onlineUserBy.comp_cod = params.cmp_id.trim();
-                    // user.onlineUserBy.hotel_cod = user.hotel_cod;
-                    // user.onlineUserBy.availUserNum = 2;
-                    // mongoAgent.OnlineUser.findOne(user.onlineUserBy, function (err, result) {
-                    //     if (!result) {
-                    //         let lo_saveData = {
-                    //             athena_id: user.onlineUserBy.athena_id,
-                    //             comp_cod: user.onlineUserBy.comp_cod,
-                    //             hotel_cod: user.onlineUserBy.hotel_cod,
-                    //             onlineUserSession: [],
-                    //             availUserNum: user.onlineUserBy.availUserNum,
-                    //             lastUpdTime: new Date()
-                    //         };
-                    //         mongoAgent.OnlineUser(lo_saveData).save(function (err) {
-                    //             if (err) {
-                    //                 callback(err, "1111", null);
-                    //             }
-                    //             else {
-                    //                 callback(null, "", user);
-                    //             }
-                    //         });
-                    //     }
-                    //     else {
-                    //         let lo_cond = {
-                    //             "athena_id": user.onlineUserBy.athena_id,
-                    //             "comp_cod": user.onlineUserBy.comp_cod,
-                    //             "hotel_cod": user.onlineUserBy.hotel_cod
-                    //         };
-                    //         mongoAgent.OnlineUser.update(lo_cond, {availUserNum: user.onlineUserBy.availUserNum}, function (err) {
-                    //             if (err) {
-                    //                 callback(err, "1111", null);
-                    //             }
-                    //             else {
-                    //                 callback(null, "", user);
-                    //             }
-                    //         });
-                    //     }
-                    // });
+                    user.onlineUserBy = {};
+                    queryAgent.queryList("QRY_AVAIL_USER_NUM", {athena_id: user.athena_id}, 0, 0, function (err, result) {
+                        if (err) {
+                            callback(err, "555", null);
+                        }
+                        else {
+                            //上線人數以集團為主
+                            if (result.length == 1) {
+                                user.onlineUserBy.athena_id = result[0].athena_id;
+                                user.onlineUserBy.comp_cod = _.isNull(result[0].comp_cod) ?
+                                    "" : result[0].comp_cod;
+                                user.onlineUserBy.hotel_cod = _.isNull(result[0].hotel_cod) ?
+                                    "" : result[0].hotel_cod;
+                                user.onlineUserBy.availUserNum = Number(result[0].concurrent_user);
+                            }
+                            else {
+                                _.each(result, function (lo_result, idx) {
+                                    result[idx].comp_cod = _.isNull(lo_result.comp_cod) ? "" : lo_result.comp_cod.trim();
+                                    result[idx].hotel_cod = _.isNull(lo_result.hotel_cod) ? "" : lo_result.hotel_cod.trim();
+                                });
+                                let lo_userNumField = _.findIndex(result, {hotel_cod: user.hotel_cod});
+                                //上線人數以館別為主
+                                if (lo_userNumField > -1) {
+                                    user.onlineUserBy.athena_id = result[lo_userNumField].athena_id;
+                                    user.onlineUserBy.comp_cod = result[lo_userNumField].comp_cod;
+                                    user.onlineUserBy.hotel_cod = result[lo_userNumField].hotel_cod;
+                                    user.onlineUserBy.availUserNum = Number(result[lo_userNumField].concurrent_user);
+                                }
+                                //上線人數以公司別為主
+                                else {
+                                    lo_userNumField = _.findIndex(result, {comp_cod: params.cmp_id});
+                                    if (lo_userNumField > -1) {
+                                        user.onlineUserBy.athena_id = result[lo_userNumField].athena_id;
+                                        user.onlineUserBy.comp_cod = _.isNull(result[lo_userNumField].comp_cod) ?
+                                            "" : result[lo_userNumField].comp_cod;
+                                        user.onlineUserBy.hotel_cod = result[lo_userNumField].hotel_cod;
+                                        user.onlineUserBy.availUserNum = Number(result[lo_userNumField].concurrent_user);
+                                    }
+                                    else {
+                                        callback(err, "1111", null);
+                                    }
+                                }
 
-                    /**
-                     * 正式
-                     */
-                    // queryAgent.queryList("QRY_AVAIL_USER_NUM", {athena_id: user.athena_id}, 0, 0, function(err, result){
-                    //     if(err){
-                    //         callback(err, "555", null);
-                    //     }
-                    //     else{
-                    //         //上線人數以集團為主
-                    //         if(result.length == 1){
-                    //             user.onlineUserBy.athena_id = result[0].athena_id;
-                    //             user.onlineUserBy.comp_cod = result[0].comp_cod;
-                    //             user.onlineUserBy.hotel_cod = result[0].hotel_cod;
-                    //             user.onlineUserBy.availUserNum = result[0].current_user;
-                    //         }
-                    //         else{
-                    //             // let lo_userNumField =  _.findIndex(result, {hotel_cod: user.hotel_cod});
-                    //             // //上線人數以館別為主
-                    //             // if(lo_userNumField > -1){
-                    //             //     user.onlineUserBy.athena_id = result[lo_userNumField].athena_id;
-                    //             //     user.onlineUserBy.comp_cod = result[lo_userNumField].comp_cod;
-                    //             //     user.onlineUserBy.hotel_cod = result[lo_userNumField].hotel_cod;
-                    //             //     user.onlineUserBy.availUserNum = result[lo_userNumField].current_user;
-                    //             // }
-                    //             // //上線人數以公司別為主
-                    //             // else{
-                    //             //     lo_userNumField = _.findIndex(result, {comp_cod: params.cmp_id});
-                    //             //     user.onlineUserBy.athena_id = result[lo_userNumField].athena_id;
-                    //             //     user.onlineUserBy.comp_cod = result[lo_userNumField].comp_cod;
-                    //             //     user.onlineUserBy.hotel_cod = result[lo_userNumField].hotel_cod;
-                    //             //     user.onlineUserBy.availUserNum = result[lo_userNumField].current_user;
-                    //             // }
-                    //
-                    //             user.onlineUserBy.athena_id = user.athena_id;
-                    //             user.onlineUserBy.comp_cod = params.cmp_id;
-                    //             user.onlineUserBy.hotel_cod = user.hotel_cod;
-                    //             user.onlineUserBy.availUserNum = 2;
-                    //         }
-                    //         //insert 到 mongo裡面
-                    //         mongoAgent.OnlineUser.findOne(user.onlineUserBy, function(result){
-                    //             if(!result){
-                    //                 let lo_saveData = {
-                    //                     athena_id: user.onlineUserBy.athena_id,
-                    //                     comp_cod: user.onlineUserBy.comp_cod,
-                    //                     hotel_cod: user.onlineUserBy.hotel_cod,
-                    //                     onlineUserSession: [],
-                    //                     availUserNum: user.onlineUserBy.availUserNum,
-                    //                     lastUpdTime: new Date()
-                    //                 };
-                    //                 mongoAgent.OnlineUser(lo_saveData).save(function (err) {
-                    //                     if (err) {
-                    //                         callback(err, "1111", null);
-                    //                     }
-                    //                     else{
-                    //                         callback(null, "", user);
-                    //                     }
-                    //                 });
-                    //             }
-                    //             else{
-                    //                 let lo_cond = {
-                    //                     "athena_id": user.onlineUserBy.athena_id,
-                    //                     "comp_cod": user.onlineUserBy.comp_cod,
-                    //                     "hotel_cod": user.onlineUserBy.hotel_cod
-                    //                 };
-                    //                 mongoAgent.OnlineUser.update(lo_cond, {availUserNum: user.onlineUserBy.availUserNum}, function (err) {
-                    //                     if (err) {
-                    //                         callback(err, "1111", null);
-                    //                     }
-                    //                     else{
-                    //                         callback(null, "", user);
-                    //                     }
-                    //                 });
-                    //             }
-                    //         });
-                    //     }
-                    // });
+                            }
+                            //insert 到 mongo裡面
+                            mongoAgent.OnlineUser.findOne(user.onlineUserBy, function (err, result) {
+                                if (!result) {
+                                    let lo_saveData = {
+                                        athena_id: user.onlineUserBy.athena_id,
+                                        comp_cod: user.onlineUserBy.comp_cod,
+                                        hotel_cod: user.onlineUserBy.hotel_cod,
+                                        onlineUserSession: [],
+                                        availUserNum: user.onlineUserBy.availUserNum,
+                                        lastUpdTime: moment(new Date())
+                                    };
+                                    mongoAgent.OnlineUser(lo_saveData).save(function (err) {
+                                        if (err) {
+                                            callback(err, "1111", null);
+                                        }
+                                        else {
+                                            callback(null, "", user);
+                                        }
+                                    });
+                                }
+                                else {
+                                    var lo_lastUpdTime = moment(new Date(moment(new Date(result.lastUpdTime)).toString()));
+                                    var ls_today = moment(new Date());
+                                    var ln_duration = moment.duration(lo_lastUpdTime.diff(ls_today));
+
+                                    let lo_cond = {
+                                        "athena_id": user.onlineUserBy.athena_id,
+                                        "comp_cod": user.onlineUserBy.comp_cod,
+                                        "hotel_cod": user.onlineUserBy.hotel_cod
+                                    };
+                                    //lastUpdTime 超過今天一天就更新mongo中的availUserNum、lastUpdTime
+                                    if (Math.abs(Number(ln_duration.asDays())) > 1) {
+                                        mongoAgent.OnlineUser.update(lo_cond, {
+                                            availUserNum: user.onlineUserBy.availUserNum,
+                                            lastUpdTime: moment(new Date())
+                                        }, function (err) {
+                                            if (err) {
+                                                callback(err, "1111", null);
+                                            }
+                                            else {
+                                                callback(null, "", user);
+                                            }
+                                        });
+                                    }
+                                    else {
+                                        callback(null, "", user);
+                                    }
+                                }
+                            });
+                        }
+                    });
                 });
             }
         });
