056b70ef91169f72a5567a8b7121c59f8f377340
a16009 <mikechen@athena.com.tw>
2018-01-26 16:21:05 +0800

    [PSIW510030] 1. app設定，取代理伺服器IP，到客戶環境測試。

--- controllers/userController.js
+++ controllers/userController.js
@@ -27,6 +27,13 @@ exports.loginPage = function (req, res) {
     let ls_account = "";
     let clientIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     clientIP = clientIP.substr(clientIP.lastIndexOf(':') + 1);
+
+    console.log("headers = " + JSON.stringify(req.headers));// 包含了各种header，包括x-forwarded-for(如果被代理过的话)
+    console.log("x-forwarded-for = " + req.header('x-forwarded-for'));// 各阶段ip的CSV, 最左侧的是原始ip
+    console.log("ips = " + JSON.stringify(req.ips));// 相当于(req.header('x-forwarded-for') || '').split(',')
+    console.log("remoteAddress = " + req.connection.remoteAddress);// 未发生代理时，请求的ip
+    console.log("ip = " + req.ip);// 同req.connection.remoteAddress, 但是格式要好一些
+
     try {
         async.waterfall([
             function (callback) {
