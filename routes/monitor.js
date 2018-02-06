8f63c987d8e20bf8246e70fc1d4de188dc56658b
a14017 <chang.maz@athena.com.tw>
2018-02-06 13:44:55 +0800

    [修正] 更改檢測程式的Router位置

--- routes/monitor.js
+++ routes/monitor.js
@@ -5,5 +5,5 @@ var monitor = require("../controllers/monitorController");
 module.exports = function (app) {
     //登入頁面
     app.get('/monitor', monitor.monitor);
-    app.get('/checkServer', monitor.checkServer);
+    app.get('/checkServerSta', monitor.checkServerSta);
 };
