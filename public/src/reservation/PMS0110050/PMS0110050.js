d841fb415faf8f879c1c959913763806b82ad941
allen830125 <allen830125@gmail.com>
2018-02-12 13:38:12 +0800

    [PMS0110050 依房號訂房] -新增 依房號訂房

---
+++ public/src/reservation/PMS0110050/PMS0110050.js
@@ -0,0 +1,166 @@
+import _s from "underscore.string";
+
+new Vue({
+    el: '#PMS0110050App',
+    mounted() {
+        this.fetchRentCalDat();
+        this.fetchData();
+    },
+    watch: {
+        searchData: {
+            handler(val) {
+                this.isLoading = true;
+                this.fetchData();
+            },
+            deep: true
+        }
+    },
+    data: {
+        isLoading: true,
+        //滾房租日
+        rentCalDat: "",
+        //搜尋日期
+        searchData: {
+            year: moment(new Date()).format("YYYY/MM/DD").toString().split("/")[0],
+            month: "03",
+            date: "24"
+        },
+        nowSearchDate: "",
+        searchData4Month: moment(new Date()).format("YYYY/MM/DD").toString(),
+        //日期欄位資料
+        dateFieldData: [],
+        dayFieldData: [],
+        //開始數與結束數
+        beginNum: "",
+        endNum: "",
+        numFieldData: [],
+        //房型資料
+        roomFieldData: [],
+        roomNosData: []
+    },
+    methods: {
+        //取滾房租日
+        fetchRentCalDat() {
+            $.post('/api/qryRentCalDat', {}, (result) => {
+                this.rentCalDat = result.rent_cal_dat;
+            });
+        },
+        //資料初始化
+        initData() {
+            this.dateFieldData = [];
+            this.dayFieldData = [];
+            this.roomFieldData = [];
+            this.roomNosData = [];
+            this.beginNum = "";
+            this.endNum = "";
+        },
+        fetchData() {
+            this.initData();
+
+            this.nowSearchDate = this.searchData.year + "/" + _s.rpad(this.searchData.month, 2, '0') + "/" + _s.rpad(this.searchData.date, 2, '0');
+
+            var lo_param = {
+                begin_dat: this.nowSearchDate
+            };
+
+            // $.post('/api/qryPageOneDataByRmTyp', lo_param).then(result => {
+            //     if (result.success) {
+            //         if (!_.isEmpty(result.data)) {
+            //             this.is4fieldAppear = true;
+            //             this.beginNum = result.data.date_range.begin_dat;
+            //             this.endNum = result.data.date_range.end_dat;
+            //             this.color = result.data.date_range.color;
+            //             this.roomTypData = result.data.roomTypData;
+            //             this.convertData();
+            //             this.isLoading = false;
+            //         }
+            //         else {
+            //             this.isLoading = false;
+            //             alert('查無資料');
+            //         }
+            //
+            //     }
+            // });
+            this.beginNum = 24;
+            this.endNum = 37;
+            this.roomNosData = [
+                {
+                    roomTyp: 'DXK',
+                    room_nos: '501',
+                    room_sta: 'Q', 	//清掃狀況
+                    begin_dat: 24, 	//有效開始日期
+                    end_dat: 30, 	//有效結束日期
+                    room_use: [
+                        {
+                            begin_dat: 24,
+                            end_dat: 30,
+                            ci_dat: 30,
+                            co_dat: 31,
+                            ues_rmk: "test",
+                            use_typ: "A"//使用類別，A:排房／O:住人／R:修理／S:參觀
+                        }
+                    ]
+                },
+                {
+                    roomTyp: 'DXK',
+                    room_nos: '502',
+                    room_sta: 'Q', //清掃狀況
+                    begin_dat: 31, //有效開始日期
+                    end_dat: 37,	//有效結束日期
+                    room_use: [
+                        {
+                            begin_dat: 24,
+                            end_dat: 30,
+                            ci_dat: 30,
+                            co_dat: 31,
+                            ues_rmk: "test2",
+                            use_typ: "A"//使用類別，A:排房／O:住人／R:修理／S:參觀
+                        }
+                    ]
+                }
+            ];
+            this.convertData();
+        },
+        convertData() {
+            var self = this;
+
+            //處理日期欄位資料
+            var ls_date = this.searchData.year + "/" + _s.rpad(this.searchData.month, 2, '0') + "/" + _s.rpad(this.searchData.date, 2, '0');
+            for (let i = this.beginNum; i <= this.endNum; i++) {
+                let lo_date = moment(new Date(ls_date)).add('days', i - this.beginNum);
+                this.dateFieldData.push({data: lo_date.format("YYYY/MM/DD").toString().split("/")[2]});
+                this.dayFieldData.push({data: lo_date.format("ddd")});
+            }
+
+            //處理房間有效日期
+            _.each(this.roomNosData, (lo_roomNosData) => {
+                var ln_numFieldLen = 2 * (this.endNum - this.beginNum + 1);
+                var la_tmpRoomUse = new Array(ln_numFieldLen);
+
+                let ln_count = 0;
+                let ln_pushNum = 0;
+                while (ln_count < ln_numFieldLen) {
+                    la_tmpRoomUse[ln_count] = {num: this.beginNum + ln_pushNum};
+                    la_tmpRoomUse[ln_count + 1] = {num: this.beginNum + ln_pushNum};
+                    ln_count = ln_count + 2;
+                    ln_pushNum = ln_pushNum + 1;
+                }
+
+            });
+        },
+        selectDate() {
+            var ls_date = moment(new Date(this.searchData4Month)).format("YYYY/MM/DD").toString();
+            this.searchData.year = ls_date.split("/")[0];
+            this.searchData.month = ls_date.split("/")[1];
+            this.searchData.date = ls_date.split("/")[2];
+        },
+        //搜尋日轉回滾房租日
+        backToRentCalDat() {
+            this.searchData.year = this.rentCalDat.split("/")[0];
+            this.searchData.month = this.rentCalDat.split("/")[1];
+            this.searchData.date = this.rentCalDat.split("/")[2];
+            this.searchData4Month = moment(new Date(this.rentCalDat));
+        }
+
+    }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const config = {
-    entry: "./public/src/reservation/PMS0110010/PMS0110010.js",
+    entry: "./public/src/reservation/PMS0110050/PMS0110050.js",
     // entry: "./public/src/sales/PMS0610010.js",
     // // entry: [
     // //     'webpack/hot/dev-server',
@@ -19,7 +19,7 @@ const config = {
         path: path.join(__dirname, 'public', 'js/reservation/'),
         // path: path.join(__dirname, 'public', 'js/sales/'),
         // filename: 'permissionSetup.js'
-        filename: 'PMS0110010.main.js'
+        filename: 'PMS0110050.js'
 
     },
     module: {
