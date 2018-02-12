e4a74f38acb5371631e7cb3cbc82847519779246
a17017 <pohao.wang@athena.com.tw>
2018-02-12 16:28:14 +0800

    [PMS0110050 依房號訂房] -新增 套版

--- public/src/reservation/PMS0110050/PMS0110050.js
+++ public/src/reservation/PMS0110050/PMS0110050.js
@@ -1,4 +1,5 @@
 import _s from "underscore.string";
+import _ from "underscore";
 
 new Vue({
     el: '#PMS0110050App',
@@ -92,12 +93,12 @@ new Vue({
                     end_dat: 30, 	//有效結束日期
                     room_use: [
                         {
-                            begin_dat: 24,
-                            end_dat: 30,
+                            begin_dat: 25,
+                            end_dat: 25,
                             ci_dat: 30,
                             co_dat: 31,
                             ues_rmk: "test",
-                            use_typ: "A"//使用類別，A:排房／O:住人／R:修理／S:參觀
+                            use_typ: "R"//使用類別，A:排房／O:住人／R:修理／S:參觀
                         }
                     ]
                 },
@@ -109,12 +110,20 @@ new Vue({
                     end_dat: 37,	//有效結束日期
                     room_use: [
                         {
-                            begin_dat: 24,
-                            end_dat: 30,
+                            begin_dat: 31,
+                            end_dat: 35,
                             ci_dat: 30,
                             co_dat: 31,
                             ues_rmk: "test2",
                             use_typ: "A"//使用類別，A:排房／O:住人／R:修理／S:參觀
+                        },
+                        {
+                            begin_dat: 36,
+                            end_dat: 39,
+                            ci_dat: 32,
+                            co_dat: 35,
+                            ues_rmk: "test3",
+                            use_typ: "O"//使用類別，A:排房／O:住人／R:修理／S:參觀
                         }
                     ]
                 }
@@ -133,19 +142,53 @@ new Vue({
             }
 
             //處理房間有效日期
-            _.each(this.roomNosData, (lo_roomNosData) => {
+            _.each(this.roomNosData, (lo_roomNosData, idx) => {
                 var ln_numFieldLen = 2 * (this.endNum - this.beginNum + 1);
                 var la_tmpRoomUse = new Array(ln_numFieldLen);
 
                 let ln_count = 0;
                 let ln_pushNum = 0;
                 while (ln_count < ln_numFieldLen) {
-                    la_tmpRoomUse[ln_count] = {num: this.beginNum + ln_pushNum};
-                    la_tmpRoomUse[ln_count + 1] = {num: this.beginNum + ln_pushNum};
+                    la_tmpRoomUse[ln_count] = {num: this.beginNum + ln_pushNum, isUsed: false};
+                    la_tmpRoomUse[ln_count + 1] = {num: this.beginNum + ln_pushNum, isUsed: false};
                     ln_count = ln_count + 2;
                     ln_pushNum = ln_pushNum + 1;
                 }
 
+                //處理房號使用狀況
+                _.each(lo_roomNosData.room_use, function (lo_roomUse) {
+                    let ln_beginDatIdx = _.findIndex(la_tmpRoomUse, {num: lo_roomUse.begin_dat});
+                    let ln_endDatIdx = _.findIndex(la_tmpRoomUse, {num: lo_roomUse.end_dat});
+
+                    ln_beginDatIdx = ln_beginDatIdx > -1 ? ln_beginDatIdx + 1 : 0;
+                    ln_endDatIdx = ln_endDatIdx > -1 ? ln_endDatIdx : la_tmpRoomUse.length - 1;
+
+                    la_tmpRoomUse[ln_beginDatIdx].isUsed = true;
+
+                    let ls_roomUseClass = "";
+                    //轉換房間使用類別代號
+                    lo_roomUse.use_typ = lo_roomUse.use_typ == "R" ? 'OOO' : lo_roomUse.use_typ;
+                    ls_roomUseClass = ls_roomUseClass + 'status_' + lo_roomUse.use_typ.toLocaleLowerCase();
+                    //轉換房間使用期間
+                    ls_roomUseClass = ls_roomUseClass + ' colspan-' + (ln_endDatIdx - ln_beginDatIdx + 1);
+                    if (lo_roomUse.begin_dat < self.beginNum) {
+                        ls_roomUseClass = ls_roomUseClass + ' only-oneArrow-left';
+                    }
+                    else if (lo_roomUse.end_dat > self.endNum) {
+                        ls_roomUseClass = ls_roomUseClass + ' only-oneArrow-right';
+                    }
+                    else if(lo_roomUse.end_dat == lo_roomUse.begin_dat){
+                        ls_roomUseClass = ls_roomUseClass + ' only-oneArrow-right';
+                    }
+
+                    la_tmpRoomUse[ln_beginDatIdx].attClass = ls_roomUseClass;
+
+                    //轉換房間使用備註
+                    la_tmpRoomUse[ln_beginDatIdx].use_rmk = lo_roomUse.ues_rmk;
+
+                });
+
+                this.roomNosData[idx].room_use = la_tmpRoomUse;
             });
         },
         selectDate() {
