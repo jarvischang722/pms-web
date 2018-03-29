f5905610327c7380fed810c023ee06c8ca80d519
m15009 <jwc1116@athena.com.tw>
2018-03-29 13:39:54 +0800

    [切版] 新增客戶維護頁面

---
+++ routes/customerSer.js
@@ -0,0 +1,15 @@
+/**
+ * Created by jing on 2018/03/29.
+ */
+var customerSerCrtl = require("../controllers/customerSerController");
+var authMW = require("../middlewares/authMiddleware");
+var sysMW = require("../middlewares/systemMiddleware");
+var i18nMW = require("../middlewares/i18nMiddleware");
+var middles = [i18nMW, authMW, sysMW];
+
+/* GET  page. */
+module.exports = function (app) {
+
+    //客戶維護設定(靜態)
+    app.get('/SER0110010', middles, customerSerCrtl.getSER0110010);
+};
