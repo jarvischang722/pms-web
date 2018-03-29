420677cec90c39085f784f8f2487697cfc391bba
a16010 <lai.kaiyue@athena.com.tw>
2018-03-29 10:13:25 +0800

    移除測試功能

--- services/common/saveDataAdapter.js
+++ services/common/saveDataAdapter.js
@@ -132,14 +132,14 @@ class saveDataAdapter {
         if (la_tmpCudData.length > 0) {
             try {
                 _.each(la_tmpCudData, (lo_tmpCudData, index) => {
-                    let lo_template_id = _.findWhere(tmpRf, {
+                    let ls_template_id = _.findWhere(tmpRf, {
                         page_id: Number(lo_tmpCudData.page_id),
                         tab_page_id: Number(lo_tmpCudData.tab_page_id)
                     });
-                    let ls_template_id = "";
-                    if(_.isUndefined(lo_template_id)){
-                        ls_template_id = lo_template_id.template_id;
-                    }
+                    // let ls_template_id = "";
+                    // if(_.isUndefined(lo_template_id)){
+                    //     ls_template_id = lo_template_id.template_id;
+                    // }
 
                     if (ls_template_id.toLocaleLowerCase() == "special") {
                         let ls_rule_func_name = `${this.params.prg_id}_templateRf`;
