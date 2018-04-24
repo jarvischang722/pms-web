abc724e7cc2187e388ca0be7ae7ccd8963cff859
josephyys <josephyys@gmail.com>
2018-04-24 20:49:48 +0800

    1.改讀讀 program 裡的prg_id 	2.用eslint調整格式

--- utils/i18nHook.js
+++ utils/i18nHook.js
@@ -1,5 +1,8 @@
-
-var _i18n;
+/**
+ * Created by Joseph Yang on 2018/4/19.
+ * hook i18n translate
+ */
+let _i18n;
 
 
 /**
@@ -8,39 +11,49 @@ var _i18n;
  * @return {Object} i18n with hooked function
  * **/
 exports.hookTranslate = function (i18n) {
-
   _i18n = i18n;
 
-  var translateHandler = {
+  let propHandler = {
     get: function(obj, prop) {
 
       return prop in obj ?
         obj[prop] :
-        ("_SystemCommon" in obj && prop in obj._SystemCommon ?
-          obj._SystemCommon[prop]: _i18n.fn__.bind(this)("SystemCommon")[prop]);
+        "_SystemCommon" in obj && prop in obj._SystemCommon ?
+          obj._SystemCommon[prop]: _i18n.fn__.bind(this)("SystemCommon")[prop];
+    }
+  };
+
+
+  let pidHandler = {
+    get: function(obj, prop) {
+      if (prop in obj) {
+        obj[prop]["_SystemCommon"] = obj._SystemCommon;
+
+        return new Proxy(obj[prop], propHandler);
+      } else {
+
+        return "_SystemCommon" in obj ?
+          obj._SystemCommon : _i18n.fn__.bind(this)("SystemCommon");
+      }
     }
   };
 
   function translate(phrase){
+    let keys = phrase.split(".");
 
-    let keys =  phrase.split(".");
+    if(keys[0]!=="program")
+      {return _i18n.fn__.bind(this)(phrase);}
 
-    if(keys[0]==="SystemCommon" || keys[0]==="program")
-      return _i18n.fn__.bind(this)(phrase);
+    //assert keys.length==1
 
-    //check if defined
     let translated = _i18n.fn__.bind(this)(phrase);
+    translated._SystemCommon = _i18n.fn__.bind(this)("SystemCommon");
 
-    if(translated===phrase)
-      translated =  _i18n.fn__.bind(this)(phrase.replace(keys[0], "SystemCommon"));
-    else
-        translated._SystemCommon = _i18n.fn__.bind(this)("SystemCommon");
-
-    return new Proxy(translated, translateHandler);
+    return new Proxy(translated, pidHandler);
   }
 
   i18n.fn__ = i18n.__;
   i18n.__ = translate;
 
   return i18n;
-}
+};
