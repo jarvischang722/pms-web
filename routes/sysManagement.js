1227f7169e452fe29ccbc2360d6f16009d9c29a5
m15009 <jwc1116@athena.com.tw>
2018-04-26 17:04:35 +0800

    [切版] 使用者異動紀錄 ＆ RateCode & 排房作業 <使用者異動紀錄> 改變單號跟路徑 <排房> 點擊條件後方打勾，保留最上面選擇名稱 彈窗名稱調整 Assign All 欄位調整
    <RateCode>
    修改table樣式

--- routes/sysManagement.js
+++ routes/sysManagement.js
@@ -1,7 +1,7 @@
 /**
  * Created by jing on 2018/03/29.
  */
-var customerSerCrtl = require("../controllers/customerSerController");
+var sysManagementCrtl = require("../controllers/sysManagementController");
 var authMW = require("../middlewares/authMiddleware");
 var sysMW = require("../middlewares/systemMiddleware");
 var i18nMW = require("../middlewares/i18nMiddleware");
@@ -9,8 +9,6 @@ var middles = [i18nMW, authMW, sysMW];
 
 /* GET  page. */
 module.exports = function (app) {
-
-    //客戶維護設定(靜態)
-    app.get('/SER0140010', middles, customerSerCrtl.getSER0140010);
-
+    //客戶購買授權(靜態)
+    app.get('/SYS0210010', middles, sysManagementCrtl.getSYS0210010);
 };
