13dc191b02b31899db5069ea2d61fb8be1546977
a16010 <Yue.810810>
2018-04-27 18:32:46 +0800

    [PMS0110040訂房卡多筆] 1.QRY_ORDER_MN sql修改 2.DB.js底層新增判斷condition有沒有 '.'的欄位並取代

--- plugins/kplug-oracle/DB.js
+++ plugins/kplug-oracle/DB.js
@@ -298,7 +298,7 @@ DB.prototype.queryDao = function (dao, param, cb) {
                 }
             }
             else if (parameter.kind == 3) {
-                if (sql.indexOf(prefix + parameter.key) >= 0) {
+                if (sql.search(prefix + parameter.key) >= 0 && param[parameter.key] != undefined) {
                     if (parameter.type.toLowerCase() == 'instring') {
                         if (param[parameter.key] instanceof Array) {
                             _.each(param[parameter.key], function (ls_param, index) {
@@ -386,7 +386,7 @@ DB.prototype.doQuery = function (connection, sqlstring, condition, mode, start, 
         delete condition["inCond"];
     }
 
-    //判斷condition有沒有 '.'的欄位表示，用字串取代方法做
+    //判斷condition有沒有 '.'的欄位，用字串取代方法做
     _.each(condition, (value, paramKey) => {
         if (paramKey.indexOf(".") > -1) {
             sqlstring = sqlstring.replace(new RegExp(`:${paramKey}`, 'g'), `'${value}'`);
