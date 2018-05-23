73b7993825e2450c96a1810e47c5b929241c8225
a17017 <pohao.wang@athena.com.tw>
2018-05-23 11:55:34 +0800

    [PMS0810230 RATTE COD] -修改 房價資料顯示

--- public/src/setup/PMS0810230/components/roomTyp.vue
+++ public/src/setup/PMS0810230/components/roomTyp.vue
@@ -11,183 +11,43 @@
                                 :default-val="selectedUseTimeData" :field="{}"
                                 @change="fetchRateCodDtData">
                     </bac-select>
-                    <select class="input-medium medium-c1">
-                        <option value="-1">使用期間</option>
-                        <option value="1">2018/04/17~2018/12/31</option>
-                        <option value="2">2019/01/01~2019/12/31</option>
-                        <option value="2">2020/01/01~2020/12/31</option>
-                    </select>
                 </div>
                 <div class="right-menu-co pull-right" style="width: 80px;">
-                    <button role="button" class="btn btn-danger btn-white btn-defaultWidth">停售</button>
+                    <button role="button" class="btn btn-danger btn-white btn-defaultWidth" @click="setStopSell">
+                        {{stopSellText}}
+                    </button>
                 </div>
             </div>
             <div class="clearfix"></div>
             <div class="container_12 divider">
                 <div class="grid_12 fixed-table-container cus-resvBlockSetting-table" width="100%">
                     <!--<div class="">-->
-                    <table class="fancyTable themeTable treeControl themeTableTwo"
-                           id="PMS0810230-table" cellpadding="0" cellspacing="0">
+                    <table class="fancyTable themeTable treeControl themeTableTwo" id="PMS0810230-table" cellpadding="0" cellspacing="0">
                         <thead>
                         <tr class="grayBg">
-                            <th class="ca-headerTitle grayBg">
-                                日期規則
+                            <th class="ca-headerTitle grayBg defHt">
+                                {{i18nLang.program.PMS0810230.dateRule}}
                             </th>
-                            <th class="">STD</th>
-                            <th class="">SUP</th>
-                            <th class="">DXK</th>
-                            <th class="">DXT</th>
-                            <th class="">SUE</th>
-                            <th class="">ESD</th>
-                            <th class="">ESP</th>
-                            <th class="">EXK</th>
-                            <th class="">EXT</th>
-                            <th class="">ESU</th>
-                            <th class="">PDS</th>
+                            <th class="defHt" v-for="(value, key, index) in roomCodData4Display">{{key}}</th>
                         </tr>
                         </thead>
                         <tbody class="tbodyRight">
-                        <tr class="grayBg">
-                            <td class="middle td-first grayBg">每一天</td>
-                            <td class="numeric">20</td>
-                            <td class="numeric">20</td>
-                            <td class="numeric">22</td>
-                            <td class="numeric darkRedTxt">-1</td>
-                            <td class="numeric ">9</td>
-                            <td class="numeric ">8</td>
-                            <td class="numeric ">15</td>
-                            <td class="numeric ">10</td>
-                            <td class="numeric ">12</td>
-                            <td class="numeric ">6</td>
-                            <td class="numeric ">1</td>
+                        <tr class="grayBg" v-for="(value, key, index) in dayNamData4Display">
+                            <td class="middle td-first grayBg defHt">{{key}}</td>
+                            <template v-for="ratecodData in value">
+                                <td class="numeric defHt" @click="getData(ratecodData)" :id="ratecodData.uniKey">
+                                    <template v-if="ratecodData.isEdit && ratecodData.use_sta == 'Y'">
+                                        <input type="text" class="defHt" style="width: 100%" v-model="ratecodData.rent_amt">
+                                    </template>
+                                    <template v-else-if="ratecodData.use_sta == 'N'" style="width: 100%">
+                                        *
+                                    </template>
+                                    <template v-else style="width: 100%">
+                                        {{ratecodData.rent_amt}}
+                                    </template>
+                                </td>
+                            </template>
                         </tr>
-                        <tr class="grayBg">
-                            <td class="middle td-first grayBg">感恩節，假日</td>
-                            <td class="numeric ">20</td>
-                            <td class="numeric ">20</td>
-                            <td class="numeric ">22</td>
-                            <td class="numeric darkRedTxt">-1</td>
-                            <td class="numeric ">9</td>
-                            <td class="numeric ">8</td>
-                            <td class="numeric ">15</td>
-                            <td class="numeric ">10</td>
-                            <td class="numeric ">12</td>
-                            <td class="numeric ">6</td>
-                            <td class="numeric ">1</td>
-                        </tr>
-                        <tr>
-                            <td class="middle td-first ">國慶日</td>
-                            <td class="numeric">3800</td>
-                            <td class="numeric">4000</td>
-                            <td class="numeric ">4200</td>
-                            <td class="numeric">4500</td>
-                            <td class="numeric">1500</td>
-                            <td class="numeric ">3300</td>
-                            <td class="numeric">4500</td>
-                            <td class="numeric">4700</td>
-                            <td class="numeric">5000</td>
-                            <td class="numeric">4800</td>
-                            <td class="numeric">45000</td>
-                        </tr>
-                        <tr>
-                            <td class="middle td-first">中秋節</td>
-                            <td class="numeric">3100</td>
-                            <td class="numeric ">3300</td>
-                            <td class="numeric">3500</td>
-                            <td class="numeric">3800</td>
-                            <td class="numeric">4000</td>
-                            <td class="numeric">3600</td>
-                            <td class="numeric">3800</td>
-                            <td class="numeric">4100</td>
-                            <td class="numeric">4300</td>
-                            <td class="numeric">4300</td>
-                            <td class="numeric">40000</td>
-                        </tr>
-                        <tr>
-                            <td class="middle td-first">春節</td>
-                            <td class="numeric">3600</td>
-                            <td class="numeric">3800</td>
-                            <td class="numeric">4000</td>
-                            <td class="numeric">4300</td>
-                            <td class="numeric">4300</td>
-                            <td class="numeric">4100</td>
-                            <td class="numeric">4300</td>
-                            <td class="numeric">4500</td>
-                            <td class="numeric">4800</td>
-                            <td class="numeric">4600</td>
-                            <td class="numeric">4300</td>
-                        </tr>
-                        <tr>
-                            <td class="middle td-first">清明節</td>
-                            <td class="numeric">2800</td>
-                            <td class="numeric">3000</td>
-                            <td class="numeric">3200</td>
-                            <td class="numeric">3500</td>
-                            <td class="numeric">*</td>
-                            <td class="numeric">*</td>
-                            <td class="numeric">*</td>
-                            <td class="numeric">*</td>
-                            <td class="numeric">*</td>
-                            <td class="numeric">*</td>
-                            <td class="numeric">*</td>
-                        </tr>
-                        <tr>
-                            <td class="middle td-first">端午節</td>
-                            <td class="numeric">2750</td>
-                            <td class="numeric">2950</td>
-                            <td class="numeric">3150</td>
-                            <td class="numeric">3450</td>
-                            <td class="numeric">*</td>
-                            <td class="numeric">*</td>
-                            <td class="numeric">*</td>
-                            <td class="numeric">*</td>
-                            <td class="numeric">*</td>
-                            <td class="numeric">*</td>
-                            <td class="numeric">*</td>
-                        </tr>
-                        <tr>
-                            <td class="middle td-first">教師節</td>
-                            <td class="numeric">4200</td>
-                            <td class="numeric">4400</td>
-                            <td class="numeric">4600</td>
-                            <td class="numeric">4900</td>
-                            <td class="numeric">*</td>
-                            <td class="numeric">*</td>
-                            <td class="numeric">*</td>
-                            <td class="numeric">*</td>
-                            <td class="numeric">*</td>
-                            <td class="numeric">*</td>
-                            <td class="numeric">*</td>
-                        </tr>
-                        <tr>
-                            <td class="middle td-first">228紀念日</td>
-                            <td class="numeric">9800</td>
-                            <td class="numeric">10000</td>
-                            <td class="numeric">10200</td>
-                            <td class="numeric">10500</td>
-                            <td class="numeric">*</td>
-                            <td class="numeric">*</td>
-                            <td class="numeric">*</td>
-                            <td class="numeric">*</td>
-                            <td class="numeric">*</td>
-                            <td class="numeric">*</td>
-                            <td class="numeric">*</td>
-                        </tr>
-                        <tr>
-                            <td class="middle td-first">愚人節</td>
-                            <td class="numeric">3200</td>
-                            <td class="numeric">3400</td>
-                            <td class="numeric">3600</td>
-                            <td class="numeric">3900</td>
-                            <td class="numeric">*</td>
-                            <td class="numeric">*</td>
-                            <td class="numeric">*</td>
-                            <td class="numeric">*</td>
-                            <td class="numeric">*</td>
-                            <td class="numeric">*</td>
-                            <td class="numeric">*</td>
-                        </tr>
-
                         </tbody>
                     </table>
                     <!--</div>-->
@@ -204,6 +64,7 @@
 
 <script>
     import moment from 'moment';
+    import crypto from 'crypto';
 
     export default {
         name: 'roomTyp',
@@ -238,18 +99,21 @@
             });
         },
         mounted() {
+            this.stopSellText = go_i18nLang.program.PMS0810230.stopSell;
             this.fetchRentCalDat();
         },
         data() {
             return {
                 i18nLang: go_i18nLang,
-                editingIndex: -1, //正在編輯的index
                 rentCalDat: '', //滾房租日
                 isLoading: true,
+                stopSellText: "",
                 rateCod: '',
                 //房型原始資料
                 rateCodDtData: [],
                 oriRateCodDtData: [],
+                roomCodData4Display: [],
+                dayNamData4Display: [],
                 tmpCUD: {
                     createData: [],
                     updateData: [],
@@ -260,6 +124,8 @@
                 selectedUseTimeData: "",
                 //使用期間下拉資料
                 useTimeSelectData: [],
+                //正在編輯的資料
+                editingCellData: {},
             }
         },
         watch: {
@@ -321,8 +187,15 @@
                 if (lo_params.searchCond.rate_cod != "" && lo_params.searchCond.supply_nos != "") {
                     $.post("/api/fetchDgRowData", lo_params, result => {
                         if (result.success) {
+                            //添加唯一值屬姓
+                            _.each(result.dgRowData, (lo_dgRowData, idx) => {
+                                lo_dgRowData["uniKey"] =
+                                    crypto.randomBytes(32).toString('base64').replace(/([\(\)\[\]\{\}\^\$\+\=\-\*\?\.\"\'\|\/\\])/g, "");
+                                lo_dgRowData["isEdit"] = false;
+                            });
                             this.rateCodDtData = result.dgRowData;
                             this.oriRateCodDtData = JSON.parse(JSON.stringify(result.dgRowData));
+                            //剛新增的使用期間(未入到資料庫)
                             _.each(this.$store.state.go_allData.ga_utDataGridRowsData, (lo_useTimeData) => {
                                 let la_roomCod = lo_useTimeData.room_cods.split(",");
                                 let la_commandOption = lo_useTimeData.command_option.split(",");
@@ -352,13 +225,20 @@
                                                 rent_amt: 0,
                                                 room_cod: ls_roomCod,
                                                 supply_nos: lo_useTimeData.supply_nos,
-                                                use_sta: 'Y'
+                                                use_sta: 'Y',
+                                                isEdit: false,
+                                                uniKey: crypto.randomBytes(32).toString('base64').replace(/([\(\)\[\]\{\}\^\$\+\=\-\*\?\.\"\'\|\/\\])/g, "")
                                             };
                                             this.rateCodDtData.push(lo_appendData);
                                         }
                                     })
                                 });
                             });
+                            //依照room_cod、command_option轉換成頁面上呈現
+                            let la_rateCodDtData4RoomCod = JSON.parse(JSON.stringify(this.rateCodDtData));
+                            let la_rateCodDtData4DayNam = JSON.parse(JSON.stringify(this.rateCodDtData));
+                            this.roomCodData4Display = _.groupBy(la_rateCodDtData4RoomCod, "room_cod");
+                            this.dayNamData4Display = _.groupBy(la_rateCodDtData4DayNam, "day_nam");
                         }
                         else {
                             alert(result.errorMsg);
@@ -372,6 +252,27 @@
                 let la_selectData = !_.isUndefined(lo_coFieldData) ? lo_coFieldData.selectData : [];
                 let ls_commandOption = la_selectData.length > 0 ? _.findWhere(la_selectData, {value: command_option.substring(1, 2)}).display : "";
                 return ls_commandOption;
+            },
+            getData(ratecod_data) {
+                this.stopSellText = ratecod_data.use_sta == 'N' ?
+                    go_i18nLang.program.PMS0810230.revert : go_i18nLang.program.PMS0810230.stopSell;
+                _.each(this.dayNamData4Display, (la_val, ls_key) => {
+                    _.each(la_val, (lo_val, idx) => {
+                        this.dayNamData4Display[ls_key][idx]["isEdit"] = false;
+                    })
+                });
+                let ln_editIndex = _.findIndex(this.dayNamData4Display[ratecod_data.day_nam], {uniKey: ratecod_data.uniKey});
+                if (ln_editIndex > -1) {
+                    this.dayNamData4Display[ratecod_data.day_nam][ln_editIndex]["isEdit"] = true;
+                }
+                this.editingCellData = ratecod_data;
+            },
+            setStopSell() {
+
+                let ln_editIndex = _.findIndex(this.dayNamData4Display[this.editingCellData.day_nam], {uniKey: this.editingCellData.uniKey});
+                if (ln_editIndex > -1) {
+                    this.dayNamData4Display[this.editingCellData.day_nam][ln_editIndex]["use_sta"] = this.editingCellData.use_sta == 'N' ? 'Y' : 'N';
+                }
             }
         }
     }
