65372c914f1e4cdb3cd9f7bd2141cf411afb7631
a16010 <Yue.810810>
2018-05-31 13:40:26 +0800

    [JATS-189056] trim功能流程重構

--- ruleEngine/rules/CommonRule.js
+++ ruleEngine/rules/CommonRule.js
@@ -5,7 +5,7 @@
 const moment = require("moment");
 const _ = require("underscore");
 const fs = require("fs");
-const SaveExecDataTrimRule = require("./CommonRuleLib/SaveExecDataTrimRule");
+const trimLibByPrgID = require("./CommonRuleLib/SaveExecDataTrimRule");
 
 module.exports = {
     /**
@@ -167,6 +167,29 @@ module.exports = {
      * @param session {object}
      */
     trimSaveExecData: function (params, session) {
-        SaveExecDataTrimRule(params, session);
+        let lo_newSaveExecDatas = {};
+        if (!_.isUndefined(trimLibByPrgID[params.prg_id])) {
+            lo_newSaveExecDatas = trimLibByPrgID[params.prg_id](params.saveExecDatas, session);
+        }
+        else {
+            lo_newSaveExecDatas = trimPostData(params.saveExecDatas);
+        }
+        return lo_newSaveExecDatas;
     }
 };
+
+/**
+ * 資料去空白
+ * @param tmpCUD {Object} postData資料
+ * @returns {*}
+ */
+function trimPostData(saveExecDatas) {
+    _.each(saveExecDatas, (lo_postData, ls_tmpType) => {
+        _.each(lo_postData, (ls_postData, ls_key) => {
+            if (typeof ls_postData === "string") {
+                saveExecDatas[ls_tmpType][ls_key] = ls_postData.trim();
+            }
+        });
+    });
+    return saveExecDatas;
+}
--- ruleEngine/rules/CommonRuleLib/SaveExecDataTrimRule.js
+++ ruleEngine/rules/CommonRuleLib/SaveExecDataTrimRule.js
@@ -1,38 +1,6 @@
-const _ = require("underscore");
 const prgTrimLib = {
 };
 
-/**
- * 檢查此作業是否有trim method
- * @param params {object} API格式的儲存資料
- * @param session {object}
- * @returns {*}
- */
-function chkRule(params, session) {
-    let lo_newSaveExecDatas = {};
-    if (_.isUndefined(prgTrimLib[params.prg_id])) {
-        lo_newSaveExecDatas = prgTrimLib[params.prg_id](params.saveExecDatas, session);
-    }
-    else {
-        lo_newSaveExecDatas = trimPostData(params.saveExecDatas);
-    }
-    return lo_newSaveExecDatas;
-}
 
-/**
- * 資料去空白
- * @param tmpCUD {Object} postData資料
- * @returns {*}
- */
-function trimPostData(saveExecDatas) {
-    _.each(saveExecDatas, (lo_postData, ls_tmpType) => {
-        _.each(lo_postData, (ls_postData, ls_key) => {
-            if (typeof ls_postData === "string") {
-                saveExecDatas[ls_tmpType][ls_key] = ls_postData.trim();
-            }
-        });
-    });
-    return saveExecDatas;
-}
 
-module.exports = chkRule;
+module.exports = prgTrimLib;
