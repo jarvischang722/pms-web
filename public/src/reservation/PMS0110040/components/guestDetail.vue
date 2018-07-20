bd64ca2b029149138d29387aaa3ddd90c435dc8b
a18009 <bear30921@athena.com.tw>
2018-07-20 11:33:46 +0800

    PMS0110042，檢查住客名單目前缺少的ikeySeqNos

--- public/src/reservation/PMS0110040/components/guestDetail.vue
+++ public/src/reservation/PMS0110040/components/guestDetail.vue
@@ -909,8 +909,6 @@
             },
             //新增guest mn 資料
             async addGuestMnData() {
-
-
                 // 讀取group order_dt 當前選擇的列的資料
                 this.editingGroupData = $("#orderDtTable").datagrid('getSelected');
                 let lo_groupParam = {
@@ -928,43 +926,53 @@
                 // 篩選order_dt 明細
                 let la_detailOrderDtData = _.where(this.allOrderDtRowsData, lo_groupParam);
 
-
+                // la_detailOrderDtData 有顧客明細才做
                 if (la_detailOrderDtData.length > 0) {
-
+                    let la_lossIkeySeqNosGuest = [];
+                    // order_dt明細和group guest顧客做比對抓出目前缺少的ikey_seq_nos
                     _.each(la_detailOrderDtData, (lo_orderDtData) => {
-                        // _.findWhere(this.orderDtRowsData, {ikey_seq_nos: });
-
+                        let lo_currentGuest = _.findWhere(this.guestMnRowsData[this.editingGroupDataIndex], {
+                            ikey_seq_nos: lo_orderDtData.ikey_seq_nos
+                        });
+                        if (_.isUndefined(lo_currentGuest)) {
+                            la_lossIkeySeqNosGuest.push(lo_orderDtData);
+                        }
                     });
 
+                    let la_allGuestMnData = [];
+                    _.each(this.guestMnRowsData, (la_data) => {
+                        _.each(la_data, (lo_data) => {
+                            la_allGuestMnData.push(lo_data);
+                        });
+                    });
+                    // 新增guest mn 前要做得規則
+                    let lo_chkAddRule = await BacUtils.doHttpPromisePostProxy("/api/chkPrgFuncRule", {
+                        prg_id: gs_prgId,
+                        page_id: 1,
+                        tab_page_id: 3,
+                        func_id: "0200",
+                        allRowData: la_allGuestMnData
+                    }).then(result => {
+                        return result
+                    }).catch(err => {
+                        return {success: false, errorMsg: err}
+                    });
 
-                    if (false) {
-
-                        let la_allGuestMnData = [];
-                        _.each(this.guestMnRowsData, (la_data) => {
-                            _.each(la_data, (lo_data) => {
-                                la_allGuestMnData.push(lo_data);
+                    if (lo_chkAddRule.success) {
+                        let lo_addRowData = lo_chkAddRule.defaultValues;
+                        // 新增顧客資料把目前缺少的ikey_seq_nos補齊
+                        if (la_lossIkeySeqNosGuest.length > 0) {
+                            _.each(la_lossIkeySeqNosGuest, (lo_guest) => {
+                                console.log(lo_guest.ikey_seq_nos);
                             });
-                        });
-                        //新增guest mn 前要做得規則
-                        let lo_chkAddRule = await BacUtils.doHttpPromisePostProxy("/api/chkPrgFuncRule", {
-                            prg_id: gs_prgId,
-                            page_id: 1,
-                            tab_page_id: 3,
-                            func_id: "0200",
-                            allRowData: la_allGuestMnData
-                        }).then(result => {
-                            return result
-                        }).catch(err => {
-                            return {success: false, errorMsg: err}
-                        });
+                        } else if (la_lossIkeySeqNosGuest.length === 0) {
 
-                        if (lo_chkAddRule.success) {
-                            let lo_addRowData = lo_chkAddRule.defaultValues;
-                        }
-                        else {
-                            alert(lo_chkAddRule.errorMsg);
                         }
                     }
+                    else {
+                        alert(lo_chkAddRule.errorMsg);
+                    }
+
                 }
             },
             //刪除guest mn 資料
